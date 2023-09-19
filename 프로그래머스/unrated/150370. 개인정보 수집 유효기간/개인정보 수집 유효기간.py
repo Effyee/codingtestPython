a6d9635cfb8c4191b6f5000de8da28b8da2d4f58
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2023-09-19 16:37:37 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,17 +1,16 @@
 def calc_date(today):
-    year, month, day = map(int, today.split("."))
-    return year * 12 * 28 + month * 28 + day
+    year,month,date=map(int,today.split("."))
+    return year*12*28+month*28+date
 
-def solution(today, terms, privacies):
-    answer = []
-		# 오늘 날짜 계산
-    today_date = calc_date(today)
-		# 약관 매핑
-    term_map = {term[0]: int(term[2:]) for term in terms}
-    # 만료일과 오늘 날짜 비교
+def solution(today,terms,privacies):
+    answer=[]
+    today_date=calc_date(today)
+    term_map={term[0]:int(term[2:]) for term in terms}
+    
     for index, privacy in enumerate(privacies):
-        expire = term_map[privacy[-1]]
-        date = calc_date(privacy[:-2]) + expire * 28
-        if date <= today_date:
-            answer.append(index + 1)
+        expire=term_map[privacy[-1]]
+        date=calc_date(privacy[:-2])+expire*28
+        if date<=today_date:
+            answer.append(index+1)
     return answer
+        
