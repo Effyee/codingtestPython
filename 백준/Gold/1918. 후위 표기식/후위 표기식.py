b6ecb643f950903c9955d7608df01924224102d1
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-01-18 11:07:40 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -1,29 +1,30 @@
 import sys
 
 def solution(s):
-    answer = ''
-    stack = []
+    stack=[]
+    answer=''
     for char in s:
-        if char.isalpha():  # 알파벳인 경우 출력
-            answer += char
+        if char.isalpha():
+            answer+=char
         else:
-            if char == '(':  # 여는 괄호는 스택에 푸시
+            if char=='(':
                 stack.append(char)
-            elif char in ['*', '/']:  # *, / 연산자 처리
-                while stack and stack[-1] in ['*', '/']:
-                    answer += stack.pop()
+            if char in ['*','/']:
+                while stack and stack[-1] in ['*','/']:
+                    answer+=stack.pop()
                 stack.append(char)
-            elif char in ['+', '-']:  # +, - 연산자 처리
-                while stack and stack[-1] in ['*', '/', '+', '-']:
-                    answer += stack.pop()
+            if char in ['+','-']:
+                while stack and stack[-1] in ['*','/','+','-']:
+                    answer+=stack.pop()
                 stack.append(char)
-            elif char == ')':  # 닫는 괄호를 만나면 여는 괄호가 나올 때까지 스택을 팝
+            if char==')':
                 while stack and stack[-1] != '(':
-                    answer += stack.pop()
-                stack.pop()  # 여는 괄호를 팝하여 제거
-    while stack:  # 스택에 남은 연산자를 모두 팝하여 추가
-        answer += stack.pop()
+                    answer+=stack.pop()
+                stack.pop()
+    while stack:
+        answer+=stack.pop()
     print(answer)
 
-s = sys.stdin.readline().rstrip()
+s=sys.stdin.readline().rstrip()
+
 solution(s)
