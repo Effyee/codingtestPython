6c7139feb9f639c7fa723a5809ebea27071e23b7
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-01-18 11:54:36 +0900

    [Silver II] Title: 스택 수열, Time: 1892 ms, Memory: 39744 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,33 +1,25 @@
 import sys
-
-def solution(n, sequence):
+def solution(n, l):
     stack = []
-    l = [i for i in range(1, n+1)]
+    num_list = [i for i in range(1, n + 1)]
     answer = []
-
-    for num in sequence:
-        while not stack or stack[-1] < num:
-            if l and l[0] <= num:
-                stack.append(l.pop(0))  # 여기서 l[0]을 pop 하면서 stack에 append 합니다.
+    for num in l:
+        if num_list and num_list[0] <= num:
+            while num_list and num_list[0] <= num:
+                stack.append(num_list.pop(0))
                 answer.append('+')
-            else:  # l이 비어있거나 l[0]이 num보다 크면 불가능한 경우이므로 break 합니다.
-                break
-        if stack and stack[-1] == num:  # stack의 맨 위 원소가 num과 같다면 pop 연산을 수행합니다.
+        if stack and stack[-1] == num:
             stack.pop()
             answer.append('-')
-        else:  # stack의 맨 위 원소가 num과 다르면 주어진 수열을 만들 수 없습니다.
-            return 'NO'
-
-    return '\n'.join(answer)  # 모든 연산이 성공적으로 끝났다면, 연산 과정을 문자열로 반환합니다.
-
-
+        else:
+            print('NO')
+            return  # 'No'가 출력되면 함수를 바로 종료합니다.
+    print('\n'.join(answer))
 
 
-n = int(sys.stdin.readline().rstrip())
-sequence = []
+n=int(input())
+l=[]
+for i in range(n):
+    l.append(int(sys.stdin.readline().rstrip()))
 
-
-for _ in range(n):
-    sequence.append(int(sys.stdin.readline().rstrip()))
-
-print(solution(n,sequence))
+solution(n, l)
