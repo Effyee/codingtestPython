fcdbeb9dad015701c49b1ca73aeabac3ad78da90
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-01-21 16:19:06 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,26 +1,26 @@
 import sys
 
-n = int(sys.stdin.readline())
-stack = []
-
+n=int(sys.stdin.readline().rstrip())
+stack=[]
 for _ in range(n):
-    command = sys.stdin.readline().split()
+    command=sys.stdin.readline().split()
 
-    if command[0] == 'push':
+    if command[0]=='push':
         stack.append(int(command[1]))
-    elif command[0] == 'pop':
+    elif command[0]=='pop':
         if stack:
             print(stack.pop())
         else:
             print(-1)
-    elif command[0] == 'size':
+    elif command[0]=='size':
         print(len(stack))
-    elif command[0] == 'empty':
+    elif command[0]=='empty':
         if not stack:
             print(1)
         else:
             print(0)
-    elif command[0] == 'top':
+
+    elif command[0]=='top':
         if stack:
             print(stack[-1])
         else:
