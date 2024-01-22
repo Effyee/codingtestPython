6313b9b08c230837e22801d0d67b32be4635649e
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-01-22 18:47:45 +0900

    [Silver III] Title: 후위 표기식2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -1,25 +1,27 @@
-n = int(input())
-expression = input()
+import sys
+n=int(sys.stdin.readline().rstrip())
+expression=sys.stdin.readline().rstrip()
 values = [int(input()) for _ in range(n)]
 
-# 피연산자에 해당하는 값을 사전으로 매핑합니다.
-operand_values = {chr(65 + i): values[i] for i in range(n)}
+dic={chr(65+i):values[i] for i in range(n)}
 
-stack = []
-for char in expression:
-    if char.isalpha():  # 피연산자인 경우
-        stack.append(operand_values[char])
-    else:  # 연산자인 경우
-        b = stack.pop()
-        a = stack.pop()
-        if char == '+':
-            stack.append(a + b)
-        elif char == '-':
-            stack.append(a - b)
-        elif char == '*':
-            stack.append(a * b)
-        elif char == '/':
-            stack.append(a / b)
+def solution(dic):
+    stack=[]
+    for char in expression:
+        if char.isalpha():
+            stack.append(dic[char])
+        else:
+            b=stack.pop()
+            a=stack.pop()
+            if char=='+':
+                stack.append(a+b)
+            elif char=='-':
+                stack.append(a-b)
+            elif char=='*':
+                stack.append(a*b)
+            else:
+                stack.append(a/b)
+    print(f"{stack[0]:.2f}")
 
-# 결과를 소수점 둘째 자리까지 출력합니다.
-print(f"{stack[0]:.2f}")
+solution(dic)
+
