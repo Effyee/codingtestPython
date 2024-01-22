c3607265e1419ff7af83b8c6681572f074e9326e
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-01-22 18:11:51 +0900

    [Silver IV] Title: 큐 2, Time: 1508 ms, Memory: 175192 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,34 +1,33 @@
 import sys
 from collections import deque
 
-def solution(command, queue):
+n=int(sys.stdin.readline().rstrip())
+
+q=deque()
+
+for _ in range(n):
+    command=sys.stdin.readline().split()
     if command[0]=='push':
-        queue.append(int(command[1]))
+        q.append(command[1])
     elif command[0]=='pop':
-        if queue:
-            print(queue.popleft())
+        if q:
+            print(q.popleft())
         else:
             print(-1)
     elif command[0]=='size':
-        print(len(queue))
+        print(len(q))
     elif command[0]=='empty':
-        if not queue:
+        if not q:
             print(1)
         else:
             print(0)
     elif command[0]=='front':
-        if queue:
-            print(queue[0])
+        if q:
+            print(q[0])
         else:
             print(-1)
-    else:  
-        if queue:
-            print(queue[-1])
+    else:
+        if q:
+            print(q[-1])
         else:
             print(-1)
-
-n=int(sys.stdin.readline().rstrip())
-queue = deque()
-for _ in range(n):
-    command=sys.stdin.readline().rstrip().split()
-    solution(command, queue)
