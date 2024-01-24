29a5df3beff2498786d2194bb27c2ad19191bb3f
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-01-24 15:00:37 +0900

    [Gold V] Title: 괄호 제거, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2800. 괄호 제거/괄호 제거.py
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.py
@@ -1,31 +1,23 @@
 import sys
 from itertools import combinations
+s=sys.stdin.readline().rstrip()
 
-n = list(map(str, sys.stdin.readline().strip()))
-answer = set()
-stack = []
-temp = []
+loc=[]
+stack=[]
+for i in range(len(s)):
+    if s[i]=='(':
+        stack.append(i)
+    elif s[i]==')':
+        loc.append((stack.pop(),i))
 
-# 반복문을 통해 괄호의 시작점과 끝점을 저장
-for idx, word in enumerate(n):
-    if word == "(":
-        stack.append(idx)
-    elif word == ")":
-        temp.append((stack.pop(), idx))
-
-for i in range(1, len(temp) + 1):
-    c = combinations(temp, i) # combinations을 통해 모든 경우의 수를 확인
-
-    # 반복문을 통해 경우의 수를 확인
-    for j in c:
-        target = list(n)
-
-        # 괄호 제거
-        for k in j:
-            target[k[0]] = ""
-            target[k[1]] = ""
-
+answer=set()
+for i in range(1, len(loc)+1):
+    for c in combinations(loc, i):
+        target = list(s)
+        for k in c:
+            target[k[0]] = ''
+            target[k[1]] = ''
         answer.add(''.join(target))
 
-for ans in sorted(list(answer)):
-    print(ans)
+answer = sorted(list(answer))
+print('\n'.join(answer))
