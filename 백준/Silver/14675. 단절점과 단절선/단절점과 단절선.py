00834cc5da111add03ce066df88f70edccd121e4
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-02-05 19:28:17 +0900

    [Silver I] Title: 단절점과 단절선, Time: 284 ms, Memory: 46236 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
@@ -1,20 +1,21 @@
 import sys
 
-n = int(input())
-tree = [[] for _ in range(n+1)]
+n=int(sys.stdin.readline().rstrip())
+tree=[[] for i in range(n+1)]
+
 for _ in range(n-1):
-    a,b = map(int,sys.stdin.readline().split())
+    a,b=map(int,sys.stdin.readline().rsplit())
     tree[a].append(b)
     tree[b].append(a)
 
-q = int(input())
-for _ in range(q):
-    t,k = map(int,sys.stdin.readline().split())
-    if t == 1:
-        if (len(tree[k]) < 2):
-            print("no")
-        else:
-            print("yes")
+q=int(sys.stdin.readline().rstrip())
 
-    elif t == 2:
-        print("yes")
+for _ in range(q):
+    t,k=map(int,sys.stdin.readline().rsplit())
+    if t==1:
+        if len(tree[k])<=1:
+            print('no')
+        else:
+            print('yes')
+    else:
+        print('yes')
