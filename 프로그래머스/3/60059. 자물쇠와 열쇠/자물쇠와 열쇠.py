7fd7ad305ebe3eb2bae2ae2cc799bfb63b16e69c
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-04-20 15:57:28 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.71 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -0,0 +1,52 @@
+def compare(board,key,x,y,M, N):
+    answer = True
+    # x,y는 key를 더할 시작 좌표
+    # M은 키의 길이로 board에 키 전체값을 더해야한다.
+    for i in range(M):
+        for j in range(M):
+            board[x + i][y + j] += key[i][j]
+
+    for i in range(N):
+        if not answer: break
+
+        for j in range(N):
+            if board[i+M][j+M] != 1:
+                answer = False
+                break
+    #  더해진 board에 다시 key 빼기
+    for i in range(M):
+        for j in range(M):
+            board[x+i][y+j] -= key[i][j]
+
+    return answer
+
+def solution(key, lock):
+
+    N, M = len(lock), len(key)
+
+    board = [[0] * (N + 2*M) for _ in range(N + 2*M)]
+    for i in range(N):
+        for j in range(N):
+            board[M+i][M+j] = lock[i][j]
+
+    for i in range(4):
+        key = rotate(key)
+
+        for i in range(1, N+M):
+            for j in range(1, N+M):
+                # board의 시작점이 i,j좌표.
+                if compare(board, key, i, j, M, N):
+                    return True
+
+    return False
+
+
+def rotate(key):
+    n = len(key)
+
+    rotate_key = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            rotate_key[j][n-i-1] = key[i][j]
+    return rotate_key
+
