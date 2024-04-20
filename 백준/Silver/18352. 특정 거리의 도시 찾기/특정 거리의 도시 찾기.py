ae44a55057da83f9fdc02cf55b4e6c6c0e4971ce
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-04-20 20:20:28 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1792 ms, Memory: 104960 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,40 +1,43 @@
 import sys
 from collections import deque
 
-#도시의 개수, 도로의 개수, 거리정보, 출발 도시 번호
-n,m,k,x=map(int,sys.stdin.readline().split())
+input=sys.stdin.readline
+
+#도시의 개수 n, 도로의 개수 m, 거리 정보 k 출발 도시 번호 x
+n,m,k,x=map(int,input().split())
 
 graph=[[] for _ in range(n+1)]
+visited=[False]*(n+1)
 
 for _ in range(m):
-    a,b=map(int,sys.stdin.readline().split())
+    a,b=map(int,input().split())
     graph[a].append(b)
 
-
-vistied=[False]*(n+1)
-distance=[[] for _ in range(n+1)]
-
-def bfs(graph,start,visited):
-    q=deque()
-    q.append((start,0))
+def bfs(start,k,visited):
+    q=deque([(start,0)])
     visited[start]=True
-
+    answer=[]
     while q:
-        v,dist=q.popleft()
-        distance[v]=dist
-        for i in graph[v]:
+        now, cost = q.popleft()
+        for i in graph[now]:
             if not visited[i]:
-                q.append((i,dist+1))
-                visited[i]=True
+                if cost+1==k:
+                    answer.append(i)
+                q.append((i, cost + 1))
+                visited[i] = True
+    return answer
 
+answer=[]
 
+answer=bfs(x,k,visited)
 
+answer.sort()
 
-bfs(graph,x,vistied)
-
-for i in range(n+1):
-    if distance[i]==k:
-        print(i)
-        
-if k not in distance:
+if len(answer)==0:
     print(-1)
+else:
+    for i in answer:
+        print(i)
+
+
+
