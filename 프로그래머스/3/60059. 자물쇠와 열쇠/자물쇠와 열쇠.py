d1eb8d1153886a4ced90b3c154cc8ccece846de4
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-04-22 17:58:58 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 1.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -3,8 +3,9 @@ def rotate(key):
     rotated = [[0] * m for _ in range(m)]
     for i in range(m):
         for j in range(m):
-            rotated[j][m-1-i] = key[i][j]
+            rotated[j][m - 1 - i] = key[i][j]
     return rotated
+
 
 def check(new_lock):
     lock_length = len(new_lock) // 3
@@ -14,28 +15,29 @@ def check(new_lock):
                 return False
     return True
 
+
 def solution(key, lock):
     n = len(lock)
     m = len(key)
-    new_lock = [[0] * (n*3) for _ in range(n*3)]
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
     # 자물쇠를 가운데에 배치
     for i in range(n):
         for j in range(n):
-            new_lock[i+n][j+n] = lock[i][j]
-    
+            new_lock[i + n][j + n] = lock[i][j]
+
     for rotation in range(4):
         key = rotate(key)
-        for x in range(n*2):
-            for y in range(n*2):
+        for x in range(n * 2):
+            for y in range(n * 2):
                 # 열쇠를 끼워 맞춰보기
                 for i in range(m):
                     for j in range(m):
-                        new_lock[x+i][y+j] += key[i][j]
+                        new_lock[x + i][y + j] += key[i][j]
                 # 자물쇠를 올바르게 열 수 있는지 확인
                 if check(new_lock):
                     return True
                 # 열쇠를 다시 빼기
                 for i in range(m):
                     for j in range(m):
-                        new_lock[x+i][y+j] -= key[i][j]
+                        new_lock[x + i][y + j] -= key[i][j]
     return False
