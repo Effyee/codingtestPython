59e7f2bf61409a49c3e9c86c125f970fe214bb19
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-04-26 11:44:37 +0900

    [Gold IV] Title: 연구소, Time: 2824 ms, Memory: 34156 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,64 +1,36 @@
-# BOJ에서는 [언어]를 PyPy3로 설정하여 제출해주세요.
+import sys
+import copy
+from collections import deque
+from itertools import combinations
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-data = [] # 초기 맵 리스트
-temp = [[0] * m for _ in range(n)] # 벽을 설치한 뒤의 맵 리스트
+graph = []
 
 for _ in range(n):
-    data.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
 
-# 4가지 이동 방향에 대한 리스트
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+def bfs(graph):
+    q = deque([(x, y) for x in range(n) for y in range(m) if graph[x][y] == 2])
 
-result = 0
+    while q:
+        x, y = q.popleft()
+        for (nx, ny) in zip ([x+1, x, x-1, x], [y, y+1, y, y-1]):
+            if 0 <= nx < n and 0 <= ny < m and not graph[nx][ny]:  # 수정된 범위 확인 부분
+                graph[nx][ny] = 2
+                q.append((nx, ny))
 
-# 깊이 우선 탐색(DFS)을 이용해 각 바이러스가 사방으로 퍼지도록 하기
-def virus(x, y):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        # 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
-        if nx >= 0 and nx < n and ny >= 0 and ny < m:
-            if temp[nx][ny] == 0:
-                # 해당 위치에 바이러스 배치하고, 다시 재귀적으로 수행
-                temp[nx][ny] = 2
-                virus(nx, ny)
+    count = sum([i.count(0) for i in graph])
+    return count  # 결과값을 반환하도록 수정
 
-# 현재 맵에서 안전 영역의 크기 계산하는 메서드
-def get_score():
-    score = 0
-    for i in range(n):
-        for j in range(m):
-            if temp[i][j] == 0:
-                score += 1
-    return score
+x_y = [(x, y) for x in range(n) for y in range(m) if not graph[x][y]]
+answer = 0
 
-# 깊이 우선 탐색(DFS)을 이용해 울타리를 설치하면서, 매 번 안전 영역의 크기 계산
-def dfs(count):
-    global result
-    # 울타리가 3개 설치된 경우
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = data[i][j]
-        # 각 바이러스의 위치에서 전파 진행
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    virus(i, j)
-        # 안전 영역의 최대값 계산
-        result = max(result, get_score())
-        return
-    # 빈 공간에 울타리를 설치
-    for i in range(n):
-        for j in range(m):
-            if data[i][j] == 0:
-                data[i][j] = 1
-                count += 1
-                dfs(count)
-                data[i][j] = 0
-                count -= 1
+for c in combinations(x_y, 3):
+    tmp = copy.deepcopy(graph)  # 깊은 복사를 사용하여 임시 그래프 생성
+    for x, y in c:
+        tmp[x][y] = 1  # 임시 그래프에 벽 설치
+    answer = max(answer, bfs(tmp))  # 임시 그래프를 bfs 함수에 전달
 
-dfs(0)
-print(result)
+print(answer)
