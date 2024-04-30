2a06a17cbf12223a043e4e49ea380d67b98ced76
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-04-30 17:31:33 +0900

    [Gold V] Title: 경쟁적 전염, Time: 108 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,26 +1,28 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
+
 n, k = map(int, input().split())
 
 graph = []
-
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
 s, x, y = map(int, input().split())
 
 def bfs():
-    virus = [(i, j, graph[i][j], 0) for i in range(n) for j in range(n) if graph[i][j] > 0]
-    virus = deque(sorted(virus, key=lambda x: x[2])) # 여기서 정렬되어 큐에 추가되어야 합니다.
+    virus = deque([(i, j, graph[i][j], 0) for i in range(n) for j in range(n) if graph[i][j] > 0])
+    virus = deque(sorted(virus, key=lambda x: x[2]))
+
     while virus:
-        x, y, virus_num, second = virus.popleft()
-        if second == s:
+        x, y, virus_num, time = virus.popleft()
+        if time == s:
             break
-        for nx, ny in ((x, y-1), (x, y+1), (x+1, y), (x-1, y)):
+        for (nx, ny) in zip([x, x, x + 1, x - 1], [y + 1, y - 1, y, y]):
             if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0:
                 graph[nx][ny] = virus_num
-                virus.append((nx, ny, virus_num, second + 1))
+                virus.append((nx, ny, virus_num, time + 1))
 
 bfs()
-print(graph[x-1][y-1])
+print(graph[x - 1][y - 1])
