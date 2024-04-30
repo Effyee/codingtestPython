a7b1a252a49dc3ec7a598819d0a57c1fa4b34a4d
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-04-30 17:18:40 +0900

    [Gold V] Title: 경쟁적 전염, Time: 108 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,6 +1,8 @@
+import sys
 from collections import deque
 
 n, k = map(int, input().split())
+
 graph = []
 
 for _ in range(n):
@@ -8,31 +10,17 @@ for _ in range(n):
 
 s, x, y = map(int, input().split())
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
 def bfs():
-    q = deque()
-    # 초기 바이러스 위치를 큐에 추가
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] != 0:
-                q.append((i, j, graph[i][j], 0))  # (x, y, virus, time)
-    # 바이러스 번호 순으로 정렬
-    q = deque(sorted(q, key=lambda x: x[2]))
-
-    while q:
-        x, y, virus, time = q.popleft()
-        if time == s:
+    virus = [(i, j, graph[i][j], 0) for i in range(n) for j in range(n) if graph[i][j] > 0]
+    virus = deque(sorted(virus, key=lambda x: x[2])) # 여기서 정렬되어 큐에 추가되어야 합니다.
+    while virus:
+        x, y, virus_num, second = virus.popleft()
+        if second == s:
             break
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = virus
-                q.append((nx, ny, virus, time + 1))
+        for nx, ny in ((x, y-1), (x, y+1), (x+1, y), (x-1, y)):
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0:
+                graph[nx][ny] = virus_num
+                virus.append((nx, ny, virus_num, second + 1))
 
 bfs()
 print(graph[x-1][y-1])
