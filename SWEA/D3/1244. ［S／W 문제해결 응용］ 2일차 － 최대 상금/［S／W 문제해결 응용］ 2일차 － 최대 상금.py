414cabdaf1e3e8b9b92c27ff028b97cb623a6e2a
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-06 17:38:31 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 428 ms, Memory: 49,404 KB -BaekjoonHub

---
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -0,0 +1,34 @@
+def dfs(n):
+    global answer
+    if N==n:
+        answer=max(answer,int(''.join(map(str,lst))))
+        return
+    for i in range(L-1):
+        for j in range(i+1,L):
+            lst[i],lst[j]=lst[j],lst[i]
+
+            chk=int(''.join(map(str,lst)))
+            if (n,chk) not in v:
+                dfs(n+1)
+                v.append((n,chk))
+   
+            lst[j],lst[i]=lst[i],lst[j]
+
+
+t=int(input())
+
+for _ in range(t):
+    numbers,n=input().split()
+
+    N=int(n)
+
+    lst,v=[],[]
+
+    for number in numbers:
+        lst.append(int(number))
+
+    L=len(lst)
+    answer=0
+    dfs(0)
+
+    print(f'#{_+1} {answer}')
