9963ae67fae0d89bf5e7ae7df1567c8c3277eb2b
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-08 18:51:18 +0900

    [D3] Title: 햄버거 다이어트, Time: 599 ms, Memory: 56,252 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,17 +1,24 @@
+def dfs(index, score, calorie):
+    global max_score, N, L, ingredients
+    # 제한 칼로리를 초과한 경우 더 이상 탐색하지 않음
+    if calorie > L:
+        return
+    # 현재까지 조합의 점수가 최대 점수보다 큰 경우 업데이트
+    if score > max_score:
+        max_score = score
+    # 모든 재료를 확인한 경우 탐색 종료
+    if index == N:
+        return
+    # 현재 재료를 선택하는 경우
+    dfs(index + 1, score + ingredients[index][0], calorie + ingredients[index][1])
+    # 현재 재료를 선택하지 않는 경우
+    dfs(index + 1, score, calorie)
+
+# 입력
 T = int(input())
-
-for case in range(1, T + 1):
-    N, L = map(int, input().split())  # 재료의 수와 제한 칼로리를 입력받음
-    ingredients = [list(map(int, input().split())) for _ in range(N)]  # 각 재료의 점수와 칼로리를 입력받음
-
-    dp = [[0] * (L + 1) for _ in range(N + 1)]  # 동적 프로그래밍을 위한 테이블 초기화
-
-    for i in range(1, N + 1):
-        Ti, Ki = ingredients[i-1]
-        for j in range(1, L + 1):
-            if Ki <= j:  # 현재 재료를 추가할 수 있는 경우
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-Ki] + Ti)
-            else:  # 현재 재료를 추가할 수 없는 경우
-                dp[i][j] = dp[i-1][j]
-
-    print(f'#{case} {dp[N][L]}')  # 결과 출력
+for t in range(1, T+1):
+    N, L = map(int, input().split())
+    ingredients = [tuple(map(int, input().split())) for _ in range(N)]
+    max_score = 0
+    dfs(0, 0, 0)
+    print(f"#{t} {max_score}")
