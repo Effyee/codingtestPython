021639c82c8b7ea43d2d42fe7ff792e2916ea69a
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-09 17:15:16 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 304 ms, Memory: 84,272 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -1,31 +1,34 @@
 from collections import deque
 
-tc = int(input())
+def bfs():
+    answer = set()
+    q = deque([])
 
-
-def bfs(graph):
-    answer = set()  
     dx = [0, 0, -1, 1]
     dy = [-1, 1, 0, 0]
 
     for i in range(4):
         for j in range(4):
-            q = deque([(i, j, graph[i][j])])  
+            q.append((i, j, graph[i][j]))
 
-            while q:
-                x, y, num = q.popleft()
-                if len(num) == 7:  
-                    answer.add(num)  
-                    continue
+    while q:
+        x, y, s = q.popleft()
+        if len(s) == 7:
+            answer.add(s)
+            continue
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < 4 and 0 <= ny < 4:
+                new_s = s + graph[nx][ny]
+                q.append((nx, ny, new_s))
 
-                for k in range(4): 
-                    nx, ny = x + dx[k], y + dy[k]
-                    if 0 <= nx < 4 and 0 <= ny < 4: 
-                        q.append((nx, ny, num + graph[nx][ny]))  
+    return len(answer)
 
-    return len(answer)  
+T = int(input())
 
+for _ in range(T):
+    graph = [list(input().split()) for _ in range(4)]
 
-for t in range(1, tc + 1):
-    graph = [input().split() for _ in range(4)]  
-    print(f'#{t} {bfs(graph)}')  
+    answer = bfs()
+
+    print(f'#{_+1} {answer}')
