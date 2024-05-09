5f38e501aa4979c6495e63e60464cdc3411dd65b
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-09 22:06:45 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 190 ms, Memory: 51,052 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def bfs(start_x, start_y):
+    q = deque([(start_x, start_y)])
+    visited[start_x][start_y] = True
+
+    dx = [0, 0, -1]  # 방향을 위로 변경
+    dy = [1, -1, 0]
+
+    while q:
+        x, y = q.popleft()
+        if x == 0:  # 가장 위에 도달했을 때, 현재 y 좌표 반환
+            return y
+        for i in range(3):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < 100 and 0 <= ny < 100 and not visited[nx][ny] and graph[nx][ny] == 1:
+                q.append((nx, ny))
+                visited[nx][ny] = True
+                break
+    return False
+
+for _ in range(10):
+    tc = int(input())
+    graph = [list(map(int, input().split())) for _ in range(100)]
+    visited = [[False] * 100 for _ in range(100)]
+
+    for i in range(100):
+        for j in range(100):
+            if graph[i][j] == 2:
+                start_x,start_y= i, j
+
+    result = bfs(start_x, start_y)
+    print("#{} {}".format(tc, result))
