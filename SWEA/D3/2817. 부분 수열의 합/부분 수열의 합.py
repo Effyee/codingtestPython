e55c1071c18dda62958c39c10babf0e93581cd97
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-12 16:22:43 +0900

    [D3] Title: 부분 수열의 합, Time: 742 ms, Memory: 60,824 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,21 +1,24 @@
-def dfs(result, cnt):
+def dfs(result,cnt):
     global answer
-    if cnt == n:  # 모든 원소를 탐색했을 경우
-        if result == k:  # 목표값에 도달했다면
-            answer += 1
+    if result==K:
+        answer+=1
+        return
+    if cnt==N:
         return
 
-    dfs(result + numbers[cnt], cnt + 1)  # 현재 원소를 포함하는 경우
-    dfs(result, cnt + 1)  # 현재 원소를 포함하지 않는 경우
+    dfs(result+numbers[cnt],cnt+1)
+    dfs(result,cnt+1)
 
-tc = int(input())
+T=int(input())
 
-for _ in range(tc):
-    n, k = map(int, input().split())  # 자연수의 개수, 목표값
-    numbers = list(map(int, input().split()))
+for _ in range(T):
+    #N(수의 개수),K(결과값)
+    N,K=map(int,input().split())
+    numbers=list(map(int,input().split()))
 
-    answer = 0
-
-    dfs(0, 0)
+    answer=0
+    dfs(0,0)
 
     print(f'#{_+1} {answer}')
+
+
