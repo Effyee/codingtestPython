c75d53fc03d4c7ae2033a0cc88f0410fb2de4bd3
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-12 18:22:07 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 106 ms, Memory: 45,248 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,21 +1,23 @@
 for _ in range(10):
-    l = int(input())
-    graph = [list(input().strip()) for _ in range(8)]
+    n=int(input())
+    graph=[list(map(str,input().rstrip())) for _ in range(8)]
 
-    answer = 0
-
-    # 가로 방향 회문 검사
+    letters=[]
+    answer=0
     for i in range(8):
-        for j in range(8 - l + 1):
-            horizontal_string = ''.join(graph[i][j:j + l])
-            if horizontal_string == horizontal_string[::-1]:  # 회문인지 확인
-                answer += 1
+        letter=''
+        for j in range(8-n+1):
+            letters.append(''.join(map(str,graph[i][j:j+n])))
 
-    # 세로 방향 회문 검사
-    for j in range(8):
-        for i in range(8 - l + 1):
-            vertical_string = ''.join(graph[k][j] for k in range(i, i + l))
-            if vertical_string == vertical_string[::-1]:  # 회문인지 확인
-                answer += 1
+    for i in range(8):
+        for j in range(8-n+1):
+            letter=''
+            for k in range(n):
+                letter+=graph[k+j][i]
+            letters.append(letter)
+
+    for letter in letters:
+        if letter==letter[::-1]:
+            answer+=1
 
     print(f'#{_+1} {answer}')
