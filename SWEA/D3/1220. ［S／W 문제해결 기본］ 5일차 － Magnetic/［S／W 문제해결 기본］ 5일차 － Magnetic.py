43a5485f5577aa5d2e3906ad001efc9af87bebe6
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-12 17:31:27 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 175 ms, Memory: 50,036 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -1,14 +1,18 @@
 for _ in range(10):
-    n = int(input())
-    graph = [list(map(int, input().split())) for _ in range(n)]
+    n=int(input())
 
-    answer = 0
+    graph=[list(map(int,input().split())) for _ in range(n)]
+
+    answer=0
+
     for i in range(n):
-        stack = []
+        stack=[]
         for j in range(n):
-            if graph[j][i] == 1:
+            if graph[j][i]==1:
                 stack.append(1)
-            elif graph[j][i] == 2 and stack: # S극 자성체를 만나고, 스택이 비어 있지 않다면(즉, N극 자성체가 있었다면)
-                answer += 1 # 교착 상태 개수 증가
-                stack = [] # 스택 초기화
+            elif graph[j][i]==2 and stack:
+                    answer+=1
+                    stack=[]
+
     print(f'#{_+1} {answer}')
+
