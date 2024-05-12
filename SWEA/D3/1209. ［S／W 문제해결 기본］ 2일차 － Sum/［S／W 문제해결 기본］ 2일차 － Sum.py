88400bea7f57740c6fdb1c007a4a2e7c76323d02
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-12 16:52:09 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 174 ms, Memory: 50,308 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,18 +1,18 @@
 for _ in range(10):
-    n=int(input())
-    graph = [list(map(int, input().split())) for _ in range(100)]
+    tc=int(input())
+    graph=[list(map(int,input().split())) for _ in range(100)]
 
-    row, col = 0, 0
-    dia1, dia2 = 0, 0  # 두 대각선의 합을 저장할 변수
+    row,col,dia1,dia2=0,0,0,0
     for i in range(100):
-        row = max(row, sum(graph[i]))  # 각 행의 합 중 최대값
-        dia1 += graph[i][i]  # 주 대각선 합
-        dia2 += graph[i][99-i]  # 부 대각선 합
+        tmp_row=sum(graph[i])
+        row=max(row,tmp_row)
+        dia1+=graph[i][i]
+        dia2+=graph[100-i-1][i]
+        tmp_col=0
+        for j in range(100):
+            tmp_col+=graph[j][i]
+            col=max(col,tmp_col)
 
-        tmp_col = sum(graph[j][i] for j in range(100))  # 각 열의 합
-        col = max(col, tmp_col)  # 각 열의 합 중 최대값
+    print(f'#{_+1} {max(row,col,dia1,dia2)}')
 
-    answer = max(row, col, dia1, dia2)  # 행, 열, 두 대각선의 합 중 최대값
-
-    print(f"#{_+1} {answer}")
 
