bc4ea4684ece4d080d1ce13bc24351d891b6698a
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-15 17:09:21 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 142 ms, Memory: 45,012 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,33 +1,30 @@
 from collections import defaultdict
 
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
 
 def inorder(node):
-    global ans
+    global answer
     if node.left:
         inorder(tree[node.left])
-    ans += node.data
+    answer+=node.data
     if node.right:
         inorder(tree[node.right])
 
-T = 10
-
-for case in range(1, T + 1):
-    ans = ""
-    N = int(input())
-    tree = defaultdict(Node)
-
-    for _ in range(N):
-        arr = list(input().split())
-        tree[int(arr[0])] = Node(arr[1])
-        if len(arr) > 2:
-            tree[int(arr[0])].left = int(arr[2])
-            if len(arr) > 3:
-                tree[int(arr[0])].right = int(arr[3])
+for _ in range(10):
+    n=int(input())
+    tree=defaultdict(Node)
+    answer=''
+    for i in range(n):
+        arr=list(input().split())
+        tree[int(arr[0])]=Node(arr[1])
+        if len(arr)>2:
+            tree[int(arr[0])].left=int(arr[2])
+            if len(arr)>3:
+                tree[int(arr[0])].right=int(arr[3])
 
     inorder(tree[1])
-    print(f"#{case} {ans}")
+    print(f'#{_+1} {answer}')
