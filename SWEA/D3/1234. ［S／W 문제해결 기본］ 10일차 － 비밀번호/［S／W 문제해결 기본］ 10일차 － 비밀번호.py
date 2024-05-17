24ab2be00e0f619951127cc41f607d6297de6d1d
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-17 22:38:59 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 102 ms, Memory: 43,956 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.py
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.py
@@ -1,20 +1,21 @@
-for tc in range(1,11):
-    n, number_string = input().split()
-    n = int(n)
-    arr = list(map(int, number_string))
-
+for _ in range(10):
+    k=list(input().split())
+    n=int(k[0])
+    arr=list(map(int,k[1].strip()))
+    i=0
     while True:
-        found = False
-
-        i = 0
-        while i < len(arr) - 1:
-            if arr[i] == arr[i + 1]:
-                del arr[i:i + 2]
-                found = True
+        while i<len(arr)-1:
+            flag = True
+            if arr[i]==arr[i+1]:
+                del arr[i:i+2]
+                i=0
+                break
             else:
-                i += 1
-
-        if not found:
+                flag=False
+            i+=1
+        else:
+            i=0
+        if flag==False:
             break
 
-    print('#'+str(tc)+' '+''.join(map(str,arr)))
+    print('#'+str(_+1)+' '+''.join(map(str,arr)))
