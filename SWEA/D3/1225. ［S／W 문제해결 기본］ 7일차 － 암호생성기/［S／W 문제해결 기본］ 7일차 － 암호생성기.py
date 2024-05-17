3cf4e7a7971ea9cf27540e0b7984cbef6250b7ad
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-17 23:10:47 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 146 ms, Memory: 49,328 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,30 +1,22 @@
 from collections import deque
 
-# 테스트 케이스는 총 10개
 for _ in range(10):
-    tc = int(input())
-    arr = list(map(int, input().split()))
+    tc=int(input())
+    arr=list(map(int,input().split()))
+    password=deque(arr)
 
-    # deque를 이용하여 큐를 초기화
-    password = deque(arr)
+    i=1
+    cnt=0
+    while password[-1]!=0:
+       if i<6:
+           result = password.popleft()-i
+           if result>0:
+               password.append(result)
+           else:
+               password.append(0)
+       else:
+           i=0
+       i+=1
+       cnt+=1
 
-    # 사이클 반복
-    n = 1
-    while True:
-        now = password.popleft()
-        now -= n
-
-        # 0보다 작거나 같으면 0으로 유지하고 종료
-        if now <= 0:
-            password.append(0)
-            break
-        else:
-            password.append(now)
-
-        # 감소 값 n은 1~5까지 반복
-        n += 1
-        if n > 5:
-            n = 1
-
-    # 결과 출력
-    print(f"#{tc}", *password)
+    print('#'+str(_+1)+' '+' '.join(map(str,password)))
