56b55ce45bf4690666d84de628db71c4396a4eb8
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-21 18:30:54 +0900

    [Gold V] Title: 암호 만들기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,34 +1,30 @@
-def dfs(result, index):
-    global passwords
-
-    if len(result) == l or index == len(word):
-        if result not in passwords and len(result) == l:
-            passwords.append(result)
+def dfs(result,index):
+    global words
+    if len(result)==l or index==c:
+        if len(result)==l and result not in words:
+            words.append(result)
         return
+    dfs(result+alphabets[index],index+1)
+    dfs(result,index+1)
 
-    dfs(result + word[index], index + 1)
-    dfs(result, index + 1)
 
 
-# 암호 길이, 문자의 종류
-l, c = map(int, input().split())
-
-word = list(map(str, input().split()))
-word.sort()
-
-passwords = []
-dfs("", 0)
-password = []
-
-for word in passwords:
-    t1, t2 = 0, 0
+l,c=map(int,input().split())
+alphabets=list(map(str,input().split()))
+words=[]
+alphabets.sort()
+dfs('',0)
+answers=[]
+for word in words:
+    t1,t2=0,0
     for w in word:
-        if w in ['a', 'e', 'i', 'o', 'u']:
-            t1 += 1
-        elif w in ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']:
-            t2 += 1
-    if t1 >= 1 and t2 >= 2:
-        password.append(word)
+        if w in ['a','e','o','u','i']:
+            t1+=1
+        else:
+            t2+=1
+    if t1>0 and t2>1:
+        answers.append(word)
 
-for p in password:
-    print(p)
+for answer in answers:
+    print(answer)
+
