8a9f52478173786f9cb5987fb8ef419bbfb3af4e
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-05-28 20:33:34 +0900

    [level 2] Title: 전화번호 목록, Time: 77.99 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,6 @@
-def solution(phoneBook):
-    phoneBook = sorted(phoneBook)
-
-    for p1, p2 in zip(phoneBook, phoneBook[1:]):
-        if p2.startswith(p1):
+def solution(phone_book):
+    phone_book.sort()
+    for i,k in zip(phone_book,phone_book[1:]):
+        if k.startswith(i):
             return False
     return True
