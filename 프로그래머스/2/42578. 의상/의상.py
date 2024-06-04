619e3d937c2ce94e5ce38500e21f4bba9759b6c6
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-04 21:17:50 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
+
 def solution(clothes):
-    answer = 0
-    dic = defaultdict(int)
-    for cloth, type in clothes:
-        dic[type] += 1
-
     answer = 1
-    for type in dic:
-        answer *= (dic[type] + 1) # 각 종류별로 옷을 입는 경우의 수 + 안 입는 경우
-
-    answer -= 1 # 모든 종류에서 아무것도 선택하지 않는 경우 제외
-    return answer
+    types=[]
+    l=defaultdict(int)
+    for c in clothes:
+        cloth,type=c
+        l[type] += 1
+        if type not in types:
+            types.append(type)
+    
+    for t in types:
+        answer*=(l[t]+1)
+    return answer-1
