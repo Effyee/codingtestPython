d8562e8c1620e4467dfd3e9002f08abddaca8075
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-05 13:43:33 +0900

    [level 2] Title: 프로세스, Time: 0.52 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,20 +1,19 @@
 def solution(priorities, location):
     answer = 0
-    i, cnt = 0, 0
-    l = []
-    for i, p in enumerate(priorities):
-        l.append((p,i ))
-    print(l)
-    while l :
-        i+=1
-        priority = max(l)
-        priority=priority[0]
-        if priority == l[0][0] and location == l[0][1]:
-            return cnt + 1
-        elif priority == l[0][0]:
-            cnt += 1
-            l.pop(0)
+    while priorities:
+        print(priorities,location,answer)
+        priority=max(priorities)
+        now=priorities.pop(0)
+        if now!=priority:
+            priorities.append(now)
+            if location-1<0:
+                location=len(priorities)-1
+            else:
+                location-=1
         else:
-            l.append(l.pop(0))
-
+            if location==0:
+                return answer+1
+            else:
+                answer+=1
+                location-=1
     return answer
