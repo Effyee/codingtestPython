214faae7cd19ed0934bc8941c36119748e0376da
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-06 15:29:25 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,26 +1,22 @@
 import heapq
 
-
 def solution(jobs):
-    jobs.sort()  # jobs를 시작 시간 기준으로 정렬
-    answer, time, cnt, waiting_time = 0, 0, 0, 0
-    now = []
-
-    i = 0
-    while cnt < len(jobs):
-        # 현재 시간까지 도달한 작업들을 now 힙에 추가
-        while i < len(jobs) and jobs[i][0] <= time:
-            heapq.heappush(now, (jobs[i][1], jobs[i][0]))
-            i += 1
-
-        if now:
-            # 가장 소요 시간이 짧은 작업을 선택
-            duration, start = heapq.heappop(now)
-            time += duration
-            waiting_time += time - start
-            cnt += 1
+    time,cnt,total_time,job=0,0,0,[]
+    jobs.sort()
+    heapq.heapify(jobs)
+    heapq.heapify(job)
+    i=0
+    while cnt<len(jobs):
+        while i<len(jobs) and jobs[i][0]<=time:
+            start,duration=jobs[i]
+            heapq.heappush(job,[duration,start])
+            i+=1
+        if job:
+            duration,start=heapq.heappop(job)
+            cnt+=1
+            time+=duration
+            total_time+=time-start
         else:
-            # 실행할 작업이 없으면 시간 증가
-            time = jobs[i][0]
+            time=jobs[i][0]
 
-    return waiting_time // len(jobs)
+    return total_time//len(jobs)
