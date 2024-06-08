dfd27d0a3ba123303fc14e6b7b8881df2215c867
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-08 16:04:18 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,40 +1,32 @@
-parent = []
-def find(x):
-    global parent
+from itertools import combinations
+
+def find_parent(x, parent):
     if parent[x] != x:
-        parent[x] = find(parent[x])
+        parent[x] = find_parent(parent[x], parent)
     return parent[x]
 
-def union(a,b):
-    global parent
-    a = find(a)
-    b = find(b)
-    
+def make_union(a, b, parent):
+    a = find_parent(a, parent)
+    b = find_parent(b, parent)
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
-        
+
 def solution(n, wires):
-    global parent
-    answer = 10000001  
+    answer = float('inf')
+    for w in combinations(wires, len(wires) - 1):
+        parent = [i for i in range(n + 1)]
+        for a, b in w:
+            make_union(a, b, parent)
+        
+        root_counts = [0] * (n + 1)
+        for i in range(1, n + 1):
+            root = find_parent(i, parent)
+            root_counts[root] += 1
+        
+        counts = [count for count in root_counts if count > 0]
+        if len(counts) == 2:
+            answer = min(answer, abs(counts[0] - counts[1]))
     
-    for i in range(len(wires)):
-        parent = [i for i in range(n+1)]
-        for j in range(len(wires)):
-            if i != j and find(wires[j][0]) != find(wires[j][1]):
-                union(wires[j][0], wires[j][1])
-        
-        group_dict = {}
-        
-        for i in range(1,n+1):
-            if find(i) not in group_dict:
-                group_dict[find(i)] = 1
-            else:
-                group_dict[find(i)] += 1
-                
-        a,b = group_dict.values()
-        
-        answer = min(answer, abs(a - b))
-        
     return answer
