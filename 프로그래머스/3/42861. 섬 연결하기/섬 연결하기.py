fc7d5b468cb31ae9ec38e0d4b3d8a5fc02fa6dcf
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-10 17:47:00 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -3,25 +3,25 @@ def find_parent(x,parent):
         parent[x]=find_parent(parent[x],parent)
     return parent[x]
 
-def make_union(parent,a,b):
+def make_union(a,b,parent):
     a=find_parent(a,parent)
     b=find_parent(b,parent)
+
     if a<b:
         parent[b]=a
     else:
         parent[a]=b
+
     return parent
 
 def solution(n, costs):
     answer = 0
     costs=sorted(costs,key=lambda x:x[2])
     parent=[i for i in range(n)]
-    print(costs,'\n',parent)
     for cost in costs:
         a,b,c=cost
-        if find_parent(a,parent)==find_parent(b,parent):
-            continue
-        answer+=c
-        if sum(make_union(parent,a,b))==0:
-            break
+        if find_parent(a,parent)!=find_parent(b,parent):
+            make_union(a,b,parent)
+            answer+=c
+
     return answer
