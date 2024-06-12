e3ce0452c38363e5cbee571f57e6593b3a35f1f7
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-12 13:12:08 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 143 ms, Memory: 49,636 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,19 +1,21 @@
-def find_max(lst):
-    max_index=0
-    for i in range(1,5):
-        if lst[max_index]<lst[i]:
-            max_index=i
-    return max_index
+def find_max(building):
+    idx = 0
+    for i in range(1, 5):
+        if building[idx] < building[i]:
+            idx = i
+    return idx
 
 for _ in range(10):
-    n=int(input())
-    buildings=list(map(int,input().split()))
-    answer=0
-    for i in range(n-4):
-        building=buildings[i:i+5]
-        sorted_building=sorted(building)
+    answer = 0
+    n = int(input())
+    buildings = list(map(int, input().split()))
 
-        if find_max(building)==2:
-            answer+=building[2]-sorted_building[-2]
+    for i in range(n - 4):
+        building = buildings[i:i+5]
+        if find_max(building) == 2:
+            m = building[2]
+            sorted_building = sorted(building)
+            result = m - sorted_building[-2]
+            answer += result
 
     print(f'#{_+1} {answer}')
