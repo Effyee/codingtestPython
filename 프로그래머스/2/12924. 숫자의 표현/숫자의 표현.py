8e9cb0d7db556fedc9239991e78244ba191a2075
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-12 14:58:08 +0900

    [level 2] Title: 숫자의 표현, Time: 3.78 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,10 +1,14 @@
 def solution(n):
     answer = 0
-    for i in range(1, n + 1):
-        sum = 0
-        while sum < n:
-            sum += i
-            i += 1
-        if sum == n:
-            answer += 1
+    for i in range(1,n+1):
+        s=0
+        num=i
+        while s<n:
+            s+=num
+            if s==n:
+                answer+=1
+                break
+            num+=1
+
+
     return answer
