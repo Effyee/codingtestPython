0dd4a40943eb7025dc7e380978c88085b9b12a59
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-12 13:16:47 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 120 ms, Memory: 46,660 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,13 +1,12 @@
-
-for _ in range(10):
+tc=10
+for _ in range(tc):
     n=int(input())
     boxes=list(map(int,input().split()))
 
     while n>0:
         boxes.sort()
-        boxes[-1]-=1
         boxes[0]+=1
+        boxes[-1]-=1
         n-=1
-
-    boxes.sort()
-    print(f'#{_+1} {boxes[-1]-boxes[0]}')
+    answer=max(boxes)-min(boxes)
+    print(f'#{_+1} {answer}')
