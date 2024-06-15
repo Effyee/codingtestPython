cccca306ad8295abec17f9ee1744d271db332e03
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-06-15 14:52:19 +0900

    [level 2] Title: 피보나치 수, Time: 7.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,10 @@
-def fib(n):
-    _curr, _next = 0, 1
-    for _ in range(n):
-        _curr, _next = _next, _curr + _next
-    return _curr
-
 def solution(n):
-    answer = 0
-    return fib(n)%1234567
+    if n==0:
+        return 0
+    if n==1:
+         return 1
+    a=0
+    b=1
+    for i in range(1,n):
+        a,b=b,(a+b)%1234567
+    return b
