321d0729f87a83ef594dea0da25e50c799c1c148
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-07-01 11:27:59 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,16 +1,19 @@
 def solution(name):
-    def min_move(name):
-        move = len(name) - 1
-        for i in range(len(name)):
-            next_i = i + 1
-            while next_i < len(name) and name[next_i] == 'A':
-                next_i += 1
-            move = min(move, i + len(name) - next_i + min(i, len(name) - next_i))
-        return move
-
     answer = 0
-    for ch in name:
-        answer += min(ord(ch) - ord('A'), ord('Z') - ord(ch) + 1)
+    n = len(name)
 
-    answer += min_move(name)
+    # 알파벳 변경 최소 횟수 계산
+    for char in name:
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+
+    # 커서 이동 최소 횟수 계산
+    min_move = n - 1
+    for i in range(n):
+        next_idx = i + 1
+        while next_idx < n and name[next_idx] == 'A':
+            next_idx += 1
+        distance = i + n - next_idx + min(i, n - next_idx)
+        min_move = min(min_move, distance)
+
+    answer += min_move
     return answer
