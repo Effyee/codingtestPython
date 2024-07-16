ef70fc19684943988a34bbabde4635b5ce1be843
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-07-16 10:31:59 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1696 ms, Memory: 105920 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -3,41 +3,32 @@ from collections import deque
 
 input=sys.stdin.readline
 
-#도시의 개수 n, 도로의 개수 m, 거리 정보 k 출발 도시 번호 x
 n,m,k,x=map(int,input().split())
-
-graph=[[] for _ in range(n+1)]
 visited=[False]*(n+1)
-
+answer=[]
+graph=[[] for _ in range(n+1)]
 for _ in range(m):
     a,b=map(int,input().split())
     graph[a].append(b)
 
-def bfs(start,k,visited):
+def bfs(start):
     q=deque([(start,0)])
     visited[start]=True
-    answer=[]
+
     while q:
-        now, cost = q.popleft()
-        for i in graph[now]:
-            if not visited[i]:
-                if cost+1==k:
-                    answer.append(i)
-                q.append((i, cost + 1))
-                visited[i] = True
-    return answer
+        now,dist=q.popleft()
+        if dist==k:
+            answer.append(now)
+        for node in graph[now]:
+            if not visited[node]:
+                visited[node]=True
+                q.append((node,dist+1))
 
-answer=[]
+bfs(x)
 
-answer=bfs(x,k,visited)
-
-answer.sort()
-
-if len(answer)==0:
-    print(-1)
+if answer:
+    answer.sort()
+    for a in answer:
+        print(a)
 else:
-    for i in answer:
-        print(i)
-
-
-
+    print(-1)
