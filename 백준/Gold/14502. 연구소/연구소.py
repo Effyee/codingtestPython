2a9b957a202d7190633e047ec04742647ebb56a1
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-07-16 11:21:26 +0900

    [Gold IV] Title: 연구소, Time: 2728 ms, Memory: 34172 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -3,34 +3,44 @@ import copy
 from collections import deque
 from itertools import combinations
 
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-graph = []
+input=sys.stdin.readline
+n,m=map(int,input().split())
+graph=[]
 
 for _ in range(n):
-    graph.append(list(map(int, input().split())))
+    graph.append(list(map(int,input().split())))
 
-def bfs(graph):
+def bfs(x,y,g):
     q = deque([(x, y) for x in range(n) for y in range(m) if graph[x][y] == 2])
 
     while q:
-        x, y = q.popleft()
-        for (nx, ny) in zip ([x+1, x, x-1, x], [y, y+1, y, y-1]):
-            if 0 <= nx < n and 0 <= ny < m and not graph[nx][ny]:  # 수정된 범위 확인 부분
-                graph[nx][ny] = 2
-                q.append((nx, ny))
+        x,y=q.popleft()
+        for dx,dy in zip([0,0,-1,1],[-1,1,0,0]):
+            nx=x+dx
+            ny=y+dy
+            if 0<=nx<n and 0<=ny<m and  g[nx][ny]==0:
+                g[nx][ny]=2
+                q.append((nx,ny))
+    return count(g)
 
-    count = sum([i.count(0) for i in graph])
-    return count  # 결과값을 반환하도록 수정
+def count(g):
+    safe_place=0
+    for i in range(len(g)):
+        for j in range(len(g[0])):
+            if g[i][j]==0:
+                safe_place+=1
+    return safe_place
 
-x_y = [(x, y) for x in range(n) for y in range(m) if not graph[x][y]]
-answer = 0
+loc=[(x,y) for x in range(n) for y in range(m) if not graph[x][y]]
 
-for c in combinations(x_y, 3):
-    tmp = copy.deepcopy(graph)  # 깊은 복사를 사용하여 임시 그래프 생성
-    for x, y in c:
-        tmp[x][y] = 1  # 임시 그래프에 벽 설치
-    answer = max(answer, bfs(tmp))  # 임시 그래프를 bfs 함수에 전달
+answer=-1
+
+for c in combinations(loc,3):
+    g=copy.deepcopy(graph)
+    for x,y in c:
+        g[x][y]=1
+    safe_area=0
+    safe_area=max(safe_area, bfs(x,y,g))
+    answer=max(answer, safe_area)
 
 print(answer)
