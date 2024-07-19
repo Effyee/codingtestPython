fb88f143d7b90bd05d5a5d1cf39d3771e5920eea
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-07-19 14:31:11 +0900

    [Gold IV] Title: 플로이드, Time: 592 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,31 +1,30 @@
 import sys
-input = sys.stdin.readline
-INF = int(1e9)
+INF=int(1e9)
+input=sys.stdin.readline
 
-# n개의 도시
-n = int(input())
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
+n=int(input())
+m=int(input())
 
-# m개의 버스
-m = int(input())
+graph=[[INF]*(n) for _ in range(n)]
 
 for _ in range(m):
-    start, dest, dist = map(int, input().split())
-    if graph[start][dest] > dist:
-        graph[start][dest] = dist
+    start,des,cost=map(int,input().split())
+    if graph[start-1][des-1]>cost:
+        graph[start-1][des-1]=cost
 
-for i in range(1, n + 1):
-    graph[i][i] = 0
+for i in range(n):
+    graph[i][i]=0
 
-for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j])
+for i in range(n):
+    for j in range(n):
+        if graph[i][j]==INF:
+            graph[i][j]=0
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if graph[i][j] == INF:
-            print(0, end=' ')
-        else:
-            print(graph[i][j], end=' ')
-    print()
+for i in range(n):
+    s=' '.join(map(str,graph[i]))
+    print(s)
+
