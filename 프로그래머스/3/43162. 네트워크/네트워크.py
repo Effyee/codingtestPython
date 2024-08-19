d67cfddf9762a6425d7e51cdb8714d03451dd0a5
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-08-19 18:40:14 +0900

    [level 3] Title: 네트워크, Time: 0.95 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,27 +1,23 @@
-def find_parent(parent, x):
+def find_parent(x, parent):
     if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
+        parent[x] = find_parent(parent[x], parent)
     return parent[x]
 
-def make_union(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
+def make_union(a, b, parent):
+    a = find_parent(a, parent)
+    b = find_parent(b, parent)
+
+    if a != b:  # a와 b가 다르면 union
         parent[b] = a
-    else:
-        parent[a] = b
 
 def solution(n, computers):
-    parent = [i for i in range(n)]
+    parent = [i for i in range(n)]  # 부모 배열 초기화
+
     for i in range(n):
         for j in range(n):
             if computers[i][j] == 1:
-                make_union(parent, i, j)
+                make_union(i, j, parent)
 
-    # 모든 노드에 대해 최종 부모를 찾는 과정
-    for i in range(n):
-        find_parent(parent, i)
-
-    # 최종 부모의 개수를 세어 네트워크의 개수를 계산
-    return len(set(parent))
-
+    # 네트워크의 개수 세기
+    network_count = len(set(find_parent(i, parent) for i in range(n)))
+    return network_count
