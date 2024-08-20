80b1253105c0c58ed027ebac1ab904ee1f2ba81a
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-08-20 18:28:51 +0900

    [level 3] Title: 단어 변환, Time: 0.44 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,24 +1,32 @@
 from collections import deque
 
-def solution(begin, target, words):
-    if target not in words:  
-        return 0
+def check(word1,word2):
+    count=0
+    for i in range(len(word1)):
+        if word1[i]!=word2[i]:
+            count+=1
+    if count==1:
+        return True
+    return False
 
-    q = deque()
-    q.append([begin, 0])
+def bfs(words,visited,begin,target):
+    q=deque()
+    q.append([begin,0])
 
     while q:
-        x, cnt = q.popleft()
+        now,count=q.popleft()
+        if now==target:
+            return count
+        for i in range(len(words)):
+            if not visited[i]:
+                if check(now,words[i]):
+                    visited[i]=True
+                    q.append((words[i],count+1))
 
-        if x == target:
-            return cnt # target값이 되면 cnt 출력
 
-        for i in range(0, len(words)):
-            diff = 0
-            word = words[i]
-            for j in range(len(x)):
-                if x[j] != word[j]:
-                    diff += 1
-            if diff == 1:
-                q.append([word, cnt + 1])
-    return 0
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    visited=[False]*len(words)
+    answer=bfs(words,visited,begin,target)
+    return answer
