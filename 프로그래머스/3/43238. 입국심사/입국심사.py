f459b1a0cec6f37c21a6ec9e24bf8bc213f9d869
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-08-23 13:30:10 +0900

    [level 3] Title: 입국심사, Time: 835.84 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,28 +1,18 @@
 def solution(n, times):
-	# 가능한 최솟값과 최댓값을 left와 right로 설정
+    answer = 0
     left = 1
     right = max(times) * n
-    
-    # 이분탐색이니 left가 right 이하인 동안
-    while left <= right:
-    	# 가운데 : 더하고 2로 나눈 몫(정수)
-        mid = (left+right)//2
-        # 심사한 사람 수
+
+    while left < right:
+        mid = (left + right) // 2
+
         people = 0
-     	
         for time in times:
-        	# 해당 심사대에서 주어진 시간동안 심사 받은 수 더하기
-            people += mid//time
-            
-            # 중간에라도 이미 n명보다 많이 심사했다면 break
-            if people >= n:
-                break
-        # n명 초과 심사했다면, 시간이 너무 많은 것
-        # 딱 n명 심사했더라도, 시간이 남을 가능성 있음
+            people += mid // time
+
         if people >= n:
-            answer = mid
-            right = mid -1
-        # n명 미만 심사했다면, 시간이 너무 부족하다
-        else :
-            left = mid + 1
-    return answer
+            right = mid  # 처리 인원이 n 이상이면 시간을 줄이기
+        else:
+            left = mid + 1  # 처리 인원이 n 미만이면 시간을 늘리기
+
+    return left  # left와 right가 만나는 지점이 최소 시간
