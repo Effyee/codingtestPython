9ad350c254f3751f6025e7ccb13e1f557c8e4788
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-08-23 13:53:43 +0900

    [level 3] Title: 가장 먼 노드, Time: 56.15 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,28 +1,42 @@
-from collections import deque
-INF=int(1e9)
-def bfs(start,distance,graph):
-    q=deque([(start)])
-    distance[start]=0
+import heapq
 
-    while q:
-        now=q.popleft()
-        for v in graph[now]:
-            if distance[v]==INF:
-                distance[v]=distance[now]+1
-                q.append(v)
-    return distance
+INF = int(1e9)
 
 
 def solution(n, edge):
-    graph=[[] for _ in range(n+1)]
-    distance=[INF]*(n+1)
-    distance[0]=0
-    for e in edge:
-        start,end=e
-        graph[start].append(end)
-        graph[end].append(start)
+    answer = 0
+    graph = [[] for _ in range(n + 1)]
 
-    print(bfs(1,distance,graph))
-    m=max(distance)
+    for start, dest in edge:
+        graph[start].append(dest)  # start에서 dest로 가는 간선 추가
+        graph[dest].append(start)  # dest에서 start로 가는 간선 추가 (양방향)
 
-    return distance.count(m)
+    distance = dijkstra(n, graph)
+
+    # 최댓값을 찾아 그 노드의 개수를 센다
+    max_distance = max(distance[1:])  # 0번 인덱스는 사용하지 않음
+    answer = distance.count(max_distance)  # 가장 멀리 있는 노드의 개수
+
+    return answer
+
+
+def dijkstra(n, graph):
+    distance = [INF] * (n + 1)
+    distance[1] = 0  # 시작 노드의 거리는 0
+    q = []
+    heapq.heappush(q, (0, 1))  # (거리, 노드)
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for v in graph[now]:  # 연결된 노드
+            cost = dist + 1  # 모든 간선의 가중치는 1
+            if cost < distance[v]:
+                distance[v] = cost
+                heapq.heappush(q, (cost, v))
+
+    return distance
+
