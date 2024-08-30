c9c2782b17b6d26f820824eecbfb397e24fe34a0
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-08-30 14:47:06 +0900

    [level 2] Title: 귤 고르기, Time: 35.49 ms, Memory: 21.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,12 +1,17 @@
+from collections import Counter
+
 def solution(k, tangerine):
     answer = 0
-    l = [0 for i in range(max(tangerine))]
-    for i in range(len(tangerine)):
-        l[tangerine[i]-1] += 1
-    l.sort(reverse = True)
+    box = Counter(tangerine)  # 귤 종류별 수를 세기 위해 Counter 사용
+    counts = sorted(box.values(), reverse=True)  # 귤의 수를 내림차순으로 정렬
 
-    index = 0
-    while answer<k:
-        answer += l[index]
-        index += 1
-    return index
+    for count in counts:
+        answer += 1  # 종류 수 증가
+        k -= count  # 현재 종류의 귤 개수만큼 빼기
+        if k <= 0:  # k가 0 이하가 되면 종료
+            break
+
+    return answer
+
+# 테스트
+print(solution(k=6, tangerine=[1, 3, 2, 5, 4, 5, 2, 3]))
