c73e34a2fdd99d322e12830893a083f77a7ac5ec
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-11-27 21:27:53 +0900

    [Gold IV] Title: 연구소, Time: 2636 ms, Memory: 35080 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,46 +1,46 @@
 import sys
-import copy
 from collections import deque
 from itertools import combinations
+import copy
 
-input=sys.stdin.readline
-n,m=map(int,input().split())
-graph=[]
+input = sys.stdin.readline
 
+n, m = map(int, input().split())
+
+graph = []
 for _ in range(n):
-    graph.append(list(map(int,input().split())))
+    graph.append(list(map(int, input().split())))
 
-def bfs(x,y,g):
-    q = deque([(x, y) for x in range(n) for y in range(m) if graph[x][y] == 2])
 
-    while q:
-        x,y=q.popleft()
-        for dx,dy in zip([0,0,-1,1],[-1,1,0,0]):
-            nx=x+dx
-            ny=y+dy
-            if 0<=nx<n and 0<=ny<m and  g[nx][ny]==0:
-                g[nx][ny]=2
-                q.append((nx,ny))
-    return count(g)
+def count_v(graph):
+    return sum(row.count(0) for row in graph)
 
-def count(g):
-    safe_place=0
-    for i in range(len(g)):
-        for j in range(len(g[0])):
-            if g[i][j]==0:
-                safe_place+=1
-    return safe_place
 
-loc=[(x,y) for x in range(n) for y in range(m) if not graph[x][y]]
+pos = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 0]
+walls = list(combinations(pos, 3))
 
-answer=-1
+answer = 0  # 최대값을 찾아야 하므로 0으로 초기화
 
-for c in combinations(loc,3):
-    g=copy.deepcopy(graph)
-    for x,y in c:
-        g[x][y]=1
-    safe_area=0
-    safe_area=max(safe_area, bfs(x,y,g))
-    answer=max(answer, safe_area)
 
+def bfs():
+    global answer
+    for wall in walls:
+        cgraph = copy.deepcopy(graph)
+        for x, y in wall:
+            cgraph[x][y] = 1
+
+        q = deque([(i, j) for i in range(n) for j in range(m) if cgraph[i][j] == 2])
+        while q:
+            x, y = q.popleft()
+            for dx, dy in zip([0, 0, -1, 1], [-1, 1, 0, 0]):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and cgraph[nx][ny] == 0:
+                    cgraph[nx][ny] = 2
+                    q.append((nx, ny))
+
+        new_answer = count_v(cgraph)
+        answer = max(answer, new_answer)
+
+
+bfs()
 print(answer)
