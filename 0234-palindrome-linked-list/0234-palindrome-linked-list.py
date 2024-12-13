d575c57c7f26917ca4ced397671e0051d0de1c0f
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2024-12-13 10:25:09 +0900

    Time: 12 ms (98.28%), Space: 39.1 MB (14.84%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,19 +3,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from collections import deque
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        q=[]
+        q=deque()
 
         if not head:
             return True
-        
         node=head
         while node is not None:
             q.append(node.val)
             node=node.next
         
         while len(q)>1:
-            if q.pop(0)!=q.pop():
+            if q.popleft()!=q.pop():
                 return False
         return True
