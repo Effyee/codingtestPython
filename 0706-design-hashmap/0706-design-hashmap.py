bc1e9699493668c11adaf08cb1ceb5a226929d2b
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-01-02 16:33:10 +0900

    Time: 32 ms (74.16%), Space: 20.8 MB (50.99%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,24 +1,16 @@
-from collections import deque
-
 class MyHashMap:
+
     def __init__(self):
-        self.hashmap = deque()  
+        self.map = {}
 
     def put(self, key: int, value: int) -> None:
-        for i in range(len(self.hashmap)):
-            if self.hashmap[i][0] == key:
-                self.hashmap[i][1] = value  
-                return
-        self.hashmap.append([key, value])  
+        self.map[key] = value
 
     def get(self, key: int) -> int:
-        for i in range(len(self.hashmap)):
-            if self.hashmap[i][0] == key:
-                return self.hashmap[i][1]
-        return -1  
+        if key not in self.map:
+            return -1
+        return self.map[key]
 
     def remove(self, key: int) -> None:
-        for i in range(len(self.hashmap)):
-            if self.hashmap[i][0] == key:
-                self.hashmap.remove(self.hashmap[i])  
-                return
+        if key in self.map:
+            self.map.pop(key)
