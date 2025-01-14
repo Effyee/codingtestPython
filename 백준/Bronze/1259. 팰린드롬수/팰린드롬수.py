2eb24593f89b497de6a65441bd20a059b701828b
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-01-14 18:55:18 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    number = input().strip()
+    if number == '0':
+        break
+    if number == number[::-1]:
+        print('yes')
+    else:
+        print('no')
