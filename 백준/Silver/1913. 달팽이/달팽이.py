55501d0c8be03553784a1071d42877a40764d0a3
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-03-24 18:07:39 +0900

    [Silver III] Title: 달팽이, Time: 836 ms, Memory: 70928 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,59 +1,45 @@
-import sys
-input = sys.stdin.readline
+N = int(input())
+number = int(input())
+snail = [[0] * N for _ in range(N)]
 
-n = int(input())
-m = int(input())
-snail = [[0]*n for _ in range(n)]
+idx, c, n = 1, 1, 3
+while idx <= N and n <= N:
+    x, y = (N - idx) // 2, (N - idx) // 2
 
-# 가운데 1 채우기
-x = (n - 1) // 2
-y = (n - 1) // 2
-snail[x][y] = 1
+    up, down, left, right = n, n - 1, n - 1, n - 2
+    snail[x][y] = c
+    up -= 1
+    c += 1
+    x -= 1
+    while right > 0:
+        snail[x][y] = c
+        c += 1
+        right -= 1
+        y += 1
+    while down > 0:
+        snail[x][y] = c
+        c += 1
+        down -= 1
+        x += 1
+    while left > 0:
+        snail[x][y] = c
+        c += 1
+        left -= 1
+        y -= 1
+    while up > 0:
+        snail[x][y] = c
+        c += 1
+        up -= 1
+        x -= 1
 
-# 방향벡터 (상, 하, 좌, 우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+    idx += 2
+    n += 2
 
-i = 2
-start = 3
+snail[0][0]=N*N
+for row in snail:
+    print(*row)
 
-while x != 0 or y != 0:
-    while i <= start * start:
-        if x == y == (n - 1) // 2: # 시작점이면 cnt 세팅, 한 칸 위로
-            cnt_up, cnt_down, cnt_left, cnt_right = start, start - 1, start - 1, start - 2
-            x += dx[0]
-            y += dy[0]
-            cnt_up -= 1
-
-        elif cnt_right > 0: # 우
-            x += dx[3]
-            y += dy[3]
-            cnt_right -= 1
-
-        elif cnt_down > 0: # 하
-            x += dx[1]
-            y += dy[1]
-            cnt_down -= 1
-
-        elif cnt_left > 0: # 좌
-            x += dx[2]
-            y += dy[2]
-            cnt_left -= 1
-
-        elif cnt_up > 0: # 상
-            x += dx[0]
-            y += dy[0]
-            cnt_up -= 1
-
-        snail[x][y] = i
-        i += 1
-
-    n -= 2
-    start += 2
-
-for j in range(len(snail)):
-    print(*snail[j])
-    if m in snail[j]:
-        mx = j + 1
-        my = snail[j].index(m) + 1
-print(mx, my)
+for x in range(N):
+    for y in range(N):
+        if snail[x][y]==number:
+            print(x+1,y+1)
