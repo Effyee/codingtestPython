1a1db1bd49c6904ccc3b9b4c488f06e080dd8f65
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-03-24 17:12:31 +0900

    [Gold V] Title: 상어 초등학교, Time: 152 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,92 +1,90 @@
 import sys
+input=sys.stdin.readline
 
-input = sys.stdin.readline
+#조건1) 좋아하는 학생인 인접한 칸에 많은
+#조건2) 1이 여러개, 인접 칸중 빈칸 많은
+#조건3) 2가 여러개, 행의 번호가 가장 작은칸
 
-N = int(input())
-students = {}
+N=int(input())
+students=[[] for _ in range((N*N)+1)]
 
-for _ in range(N * N):
-    a, b, c, d, e = map(int, input().split())
-    students[a] = {b, c, d, e}  # 좋아하는 학생들을 집합으로 저장
+graph=[[0]*(N) for _ in range(N)]
 
-graph = [[0] * N for _ in range(N)]
+def like_count(student,x,y):
+    cnt=0
+    dx=[0,0,-1,1]
+    dy=[-1,1,0,0]
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if 0<=nx<N and 0<=ny<N:
+            if graph[nx][ny] in students[student]:
+                cnt+=1
+    return cnt,x,y
 
-
-# 조건 1: 인접한 칸에 좋아하는 학생이 가장 많은 칸을 찾는 함수
-def check(x, y, student):
-    total = 0
+def vacant_count(x,y):
+    vcnt=0
     dx = [0, 0, -1, 1]
     dy = [-1, 1, 0, 0]
-
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < N and 0 <= ny < N:
-            if graph[nx][ny] in students[student]:  # 좋아하는 학생이 주변에 있는지 확인
-                total += 1
-    return total, x, y
+            if graph[nx][ny]==0:
+                vcnt += 1
+    return vcnt
 
+def solution(a):
+    lcnt = 0
+    cnd = []
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j]==0:
+                cnt,x,y=like_count(a,i,j)
+                if cnt>lcnt:
+                    lcnt=cnt
+                    cnd=[[x,y]]
+                elif cnt==lcnt:
+                    cnd.append([x,y])
+    if len(cnd)==1:
+        x,y=cnd[0][0],cnd[0][1]
+        graph[x][y]=a
+    else:
+        svcnt = -1
+        vcnd = []
+        for x,y in cnd:
+            vcnt=vacant_count(x,y)
+            if svcnt<vcnt:
+                svcnt=vcnt
+                vcnd=[[x,y]]
+            elif svcnt==vcnt:
+                vcnd.append([x,y])
 
-# 조건 2: 비어있는 칸이 가장 많은 곳 찾기
-def check_space(x, y):
-    total = 0
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
+        if len(vcnd)==1:
+            x, y = vcnd[0][0], vcnd[0][1]
+            graph[x][y] = a
+        else:
+            vcnd=sorted(vcnd,key=lambda x:(x[0],x[1]))
+            graph[vcnd[0][0]][vcnd[0][1]]=a
+for _ in range((N*N)):
+    a,b,c,d,e=map(int,input().split())
+    students[a].append(b)
+    students[a].append(c)
+    students[a].append(d)
+    students[a].append(e)
+    solution(a)
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == 0:
-            total += 1
-    return total, x, y
+answer=0
+for i in range(N):
+    for j in range(N):
+        cnt,x,y=like_count(graph[i][j],i,j)
+        if cnt==4:
+            answer+=1000
+        elif cnt==3:
+            answer+=100
+        elif cnt==2:
+            answer+=10
+        elif cnt==1:
+            answer+=1
 
-
-# 학생 배치
-for student in students.keys():  # 학생 번호 순서대로 배치
-    candidates = []
-    max_friends = -1
-
-    for x in range(N):
-        for y in range(N):
-            if graph[x][y] == 0:  # 빈 칸만 확인
-                friends, _, _ = check(x, y, student)
-
-                if friends > max_friends:
-                    max_friends = friends
-                    candidates = [(x, y)]
-                elif friends == max_friends:
-                    candidates.append((x, y))
-
-    # 좋아하는 친구가 가장 많은 칸이 하나라면 배치
-    if len(candidates) == 1:
-        x, y = candidates[0]
-        graph[x][y] = student
-        continue
-
-    # 조건 2 적용: 비어있는 칸이 가장 많은 곳 찾기
-    spaces = []
-    max_spaces = -1
-    for x, y in candidates:
-        empty_count, _, _ = check_space(x, y)
-
-        if empty_count > max_spaces:
-            max_spaces = empty_count
-            spaces = [(x, y)]
-        elif empty_count == max_spaces:
-            spaces.append((x, y))
-
-    # 최종 배치
-    x, y = sorted(spaces)[0]  # 행, 열 기준으로 정렬하여 가장 작은 위치 선택
-    graph[x][y] = student
-
-# 만족도 계산
-satisfaction = 0
-score_map = {0: 0, 1: 1, 2: 10, 3: 100, 4: 1000}
-
-for x in range(N):
-    for y in range(N):
-        student = graph[x][y]
-        friends, _, _ = check(x, y, student)
-        satisfaction += score_map[friends]
-
-print(satisfaction)
+print(answer)
