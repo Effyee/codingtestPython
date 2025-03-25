e9afc3ff7569cbdd1bead0ac994eede5047a1d7b
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-03-25 15:09:45 +0900

    [Gold IV] Title: 알파벳, Time: 6672 ms, Memory: 169168 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,23 +1,26 @@
-def dfs(x, y, bitmask, cnt):
-    global answer
-    answer = max(answer, cnt)
+import sys
+input=sys.stdin.readline
+R,C=map(int,input().split())
 
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
+graph=[]
+for _ in range(R):
+    graph.append(list(map(str,input().strip())))
 
+max_length=0
+
+def dfs(x,y,route):
+    global max_length
+    max_length=max(len(route),max_length)
+    dx=[0,0,-1,1]
+    dy=[-1,1,0,0]
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < r and 0 <= ny < c:
-            next_char = ord(graph[nx][ny]) - ord('A')
-            if not (bitmask & (1 << next_char)):
-                dfs(nx, ny, bitmask | (1 << next_char), cnt + 1)
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if 0<=nx<R and 0<=ny<C:
+            if graph[nx][ny] not in route:
+                dfs(nx,ny,route+graph[nx][ny])
 
+route=graph[0][0]
+dfs(0,0,route)
 
-r, c = map(int, input().split())
-graph = [list(input().strip()) for _ in range(r)]
-
-answer = 0
-initial_char = ord(graph[0][0]) - ord('A')
-dfs(0, 0, 1 << initial_char, 1)
-print(answer)
+print(max_length)
