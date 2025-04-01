5bdd89c25ba5d4bc13f0f148b9b4cbbb2df3cd14
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-04-01 13:04:00 +0900

    [Gold V] Title: 빗물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,25 +1,12 @@
-def calcRainwaterInColumn(c, heights):
-    # 둘러싸고 잇는 양쪽 블록 중 각각 가장 높은 블록을 구함
-    left_max = max(heights[:c])
-    right_max = max(heights[c + 1:])
-    
-    # 그 중 더 낮은 블록 높이까지 빗물이 고임
-    limit = min(left_max, right_max)
-    accumulated_rainwater = max(0, limit - heights[c])
-    
-    return accumulated_rainwater
+H, W = map(int, input().split())
+blocks = list(map(int, input().split()))
 
-def solution(H, W, heights):
-    answer = 0
-    
-    # 양 끝 열에는 고일 수 없음
-    for i in range(1, W - 1):
-        answer += calcRainwaterInColumn(i, heights)
-    
-    return answer
+answer = 0
+for i in range(1, W - 1):
+    left_max = max(blocks[:i])
+    right_max = max(blocks[i + 1:])
+    water = min(left_max, right_max) - blocks[i]
+    if water > 0:
+        answer += water
 
-if __name__ == "__main__":
-    H, W = map(int, input().split())
-    heights = list(map(int, input().split()))
-    
-    print(solution(H, W, heights))
+print(answer)
