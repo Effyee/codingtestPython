774d84cf8c60ba12ab32f2154c4ad70c5e6d94f3
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-04-02 10:56:55 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,16 +1,14 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
+dp=[0]*12
+dp[1]=1
+dp[2]=2
+dp[3]=4
 
-t = int(input())
+for i in range(4,12):
+    dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
 
-cnt = [0] * 11
-cnt[1] = 1
-cnt[2] = 2
-cnt[3] = 4
-
-for i in range(4, 11):
-    cnt[i] = cnt[i-3] + cnt[i-2] + cnt[i-1]
-
-for _ in range(t):
-    n = int(input())
-    print(cnt[n])
+T=int(input())
+for _ in range(T):
+    n=int(input())
+    print(dp[n])
