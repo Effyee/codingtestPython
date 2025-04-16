ac4586e0467d5ff2c2d4a138610b4daccd3c7260
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-04-16 10:59:58 +0900

    [Silver I] Title: 정수 삼각형, Time: 80 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,23 +1,25 @@
 import sys
+input=sys.stdin.readline
 
-n = int(sys.stdin.readline().strip())  # 첫 번째 입력
-
-dp = []
-
-for i in range(n):
-    dp.append(list(map(int, sys.stdin.readline().split())))  # 정수 삼각형 입력
+n=int(input())
+triangle=[]
+for _ in range(n):
+    triangle.append(list(map(int,input().split())))
 
 
-for i in range(1,n):
-    for j in range(i+1):
-        if j==0:
-            up_left=0
-        else:
-            up_left=dp[i-1][j-1]
-        if j==i:
-            up=0
-        else:
-            up=dp[i-1][j]
-        dp[i][j]=dp[i][j]+max(up_left,up)
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:	# 왼쪽 끝이면
+                triangle[i][j] += triangle[i-1][0]  # 이전 층의 0번째 값 더하기
+            elif j == len(triangle[i])-1:	# 오른쪽 끝이면
+                triangle[i][j] += triangle[i-1][-1]	# 이전 층의 -1번째 값 더하기
+            else:
+                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
 
-print(max(dp[n-1]))
+    return max(triangle[-1])
+
+print(solution(triangle))
+
+
+
