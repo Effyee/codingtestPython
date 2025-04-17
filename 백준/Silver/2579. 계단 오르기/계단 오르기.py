cd8af12cdf264b0870bd0753a5b6118adfe0b3a1
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-04-17 10:43:34 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,23 +1,18 @@
-def solution(n, score):
-    answer = 0
-    dp = [0] * 301
+n=int(input())
+value=[0]
+for _ in range(n):
+    value.append(int(input()))
+dp=[0]*(n+1)
 
-    dp[1] = score[1] #첫째 계단 초기 세팅(1번째 계단은 1번째 계단 밟는 게 최대)
-    dp[2] = score[1] + score[2] #둘째 계단 초기 세팅(2번째 계단은 1번째 + 2번쨰 계단 밟는 게 최대)
-
-    for i in range(3, n+1):  # 셋째 계단 이후부터 점화식 이용하여 최댓값 내기
-				#1칸 전에서 온 경우(3칸 전 최댓값에서 + 2칸 이동한 1칸 전 값), 2칸 전에서 온 경우(2칸 전의 최댓값) 중 최댓값 + 현재 계단 값
-        dp[i] = max(dp[i-3] + score[i -1], dp[i-2]) + score[i]
-
-    answer = dp[n] #최대점수가 내장되어 있는 마지막 계단의 점수값이 답
-    return answer 
-
-if __name__ == "__main__":
-    n = int(input())
-    score = [0] * 301
+def solution(n,value):
     
-    for i in range(1, n+1):
-        score[i] = int(input())
+    if n<=2:
+        return(sum(value))
+    else:
+        dp[1]=value[1]
+        dp[2]=value[1]+value[2]
+        for i in range(3,n+1):
+            dp[i]=max(dp[i-2]+value[i],dp[i-3]+value[i-1]+value[i])
+        return dp[n]
 
-    answer = solution(n,score)
-    print(answer)
+print(solution(n,value))
