f19166d420e2999ddf7a762c86960742d3acffb7
Effy__ee <97147501+gayeon7877@users.noreply.github.com>
2025-04-19 14:59:13 +0900

    [Silver I] Title: RGB거리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,19 +1,14 @@
 import sys
-from collections import deque
-
-INF=int(1e9)
-
 input=sys.stdin.readline
 
 n=int(input())
-dp=[]
+values=[]
 for _ in range(n):
-    dp.append(list(map(int,input().split())))
-
+    values.append(list(map(int,input().split())))
 
 for i in range(1,n):
-    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + dp[i][0]
-    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + dp[i][1]
-    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + dp[i][2]
+    values[i][0] = values[i][0] + min(values[i-1][1], values[i-1][2])
+    values[i][1] = values[i][1] + min(values[i-1][0], values[i-1][2])
+    values[i][2] = values[i][2] + min(values[i-1][0], values[i-1][1])
 
-print(min(dp[n-1][0],dp[n-1][1],dp[n-1][2]))
+print(min(values[n-1]))
