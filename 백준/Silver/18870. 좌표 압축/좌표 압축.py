8414b048201737f53618eb72d60cb4877bb4d064
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-04-23 18:06:27 +0900

    [Silver II] Title: 좌표 압축, Time: 1772 ms, Memory: 161372 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,21 +1,13 @@
 import sys
-from collections import defaultdict
-input=sys.stdin.readline
+from bisect import bisect_left
 
-n=int(input())
-li=list(map(int,input().split()))
-sorted_li=sorted(li)
-dict=defaultdict(int)
-ans=[]
-idx=1
-for i in range(len(sorted_li)):
-    if dict[sorted_li[i]]>0:
-        continue
-    else:
-        dict[sorted_li[i]]=idx
-        idx+=1
+input = sys.stdin.readline
 
-for num in li:
-    ans.append(dict[num]-1)
+n = int(input())
+arr = list(map(int, input().split()))
 
-print(' '.join(map(str,ans)))
+sorted_arr = sorted(set(arr))
+
+result = [str(bisect_left(sorted_arr, x)) for x in arr]
+
+print(' '.join(result))
