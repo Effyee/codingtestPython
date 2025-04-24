74781f60141591ac1a406dd6932b5fddb8952b20
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-04-24 12:12:44 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,15 +1,9 @@
 from collections import defaultdict
+from math import prod
 
 def solution(clothes):
-    answer = 1
-    types=[]
-    l=defaultdict(int)
-    for c in clothes:
-        cloth,type=c
-        l[type] += 1
-        if type not in types:
-            types.append(type)
-    
-    for t in types:
-        answer*=(l[t]+1)
-    return answer-1
+    counter = defaultdict(int)
+    for _, type_ in clothes:
+        counter[type_] += 1
+
+    return prod([count + 1 for count in counter.values()]) - 1
