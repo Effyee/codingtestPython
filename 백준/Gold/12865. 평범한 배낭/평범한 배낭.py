ced9721ea1e258571be65f8b8e3f676870f7cc74
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-04-25 11:37:42 +0900

    [Gold V] Title: 평범한 배낭, Time: 4304 ms, Memory: 228328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,20 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 n, k = map(int, input().split())
+things = [list(map(int, input().split())) for _ in range(n)]
 
-data = []
-for _ in range(n):
-    w, v = map(int, input().split())
-    data.append((w, v))
-
-knapsack = [[0] * (k + 1) for _ in range(n + 1)]
+dp = [[0] * (k + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1):
-    for j in range(1, k + 1):
-        w = data[i-1][0]
-        v = data[i-1][1]
-
-        if j < w:
-            knapsack[i][j] = knapsack[i-1][j]
+    w, v = things[i - 1]
+    for j in range(k + 1):
+        if j >= w:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
         else:
-            knapsack[i][j] = max(v + knapsack[i-1][j-w], knapsack[i-1][j])
+            dp[i][j] = dp[i - 1][j]
 
-print(knapsack[n][k])
+print(dp[n][k])
