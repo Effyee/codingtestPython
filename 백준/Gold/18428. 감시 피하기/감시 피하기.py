6528c0c7a9b6fdb9504c21f3f302a88c69403872
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-05-22 16:29:41 +0900

    [Gold V] Title: 감시 피하기, Time: 68 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,43 +1,58 @@
 import sys
-import copy
 from collections import deque
-from itertools import combinations
 input=sys.stdin.readline
 
 n=int(input())
-graph=[]
+school=[list(map(str,input().rsplit())) for _ in range(n)]
 
-for _ in range(n):
-    graph.append(list(map(str,input().split())))
+def bfs(walls):
 
-def bfs(graph):
-    q=deque([(x,y) for x in range(n) for y in range(n) if graph[x][y]=='T'])
-
+    for x,y in walls:
+        school[x][y]='O'
+    q=deque([])
+    for i in range(n):
+        for j in range(n):
+            if school[i][j]=='T':
+                q.append([i,j])
+    #왼,오,위,아래
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
     while q:
         x,y=q.popleft()
-        for dx,dy in zip([-1,1,0,0],[0,0,-1,1]):
-            nx=x+dx
-            ny=y+dy
-            while 0<=nx<n and 0<=ny<n:
-                if graph[nx][ny]=='S':
-                    return False
-                elif graph[nx][ny]=='O':
-                    break
+        for i in range(4):
+            nx,ny=x,y
+            while True:
+                nx+=dx[i]
+                ny+=dy[i]
+                if 0<=nx<n and 0<=ny<n:
+                    if school[nx][ny]=='S':
+                        for x,y in walls:
+                            school[x][y]='X'
+                        return False
+                    if school[nx][ny]=='O':
+                        break
                 else:
-                    nx+=dx
-                    ny+=dy
+                    break
+    for x, y in walls:
+        school[x][y] = 'X'
     return True
 
-locations=[(x,y) for x in range(n) for y in range(n) if graph[x][y]=='X']
+def bt(li,idx):
+    if len(li)==3:
+        if bfs(li):
+            print('YES')
+            exit()
+        return
+    if idx==n*n:
+        return
 
-def solution():
-    for c in combinations(locations,3):
-        g=copy.deepcopy(graph)
-        for x,y in c:
-            g[x][y]='O'
-        if bfs(g):
-            return 'YES'
-    return 'NO'
+    x,y=divmod(idx,n)
+    if school[x][y]=='X':
+        li.append([x,y])
+        bt(li,idx+1)
+        li.pop()
+    bt(li,idx+1)
+    return
 
-print(solution())
-
+bt([],0)
+print('NO')
