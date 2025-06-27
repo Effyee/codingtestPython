e92626225fed56cd2bdfb36a44d05bf5b296f2ac
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-06-27 10:50:30 +0900

    [Gold V] Title: LCS, Time: 432 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,14 +1,16 @@
 import sys
 input=sys.stdin.readline
 
-s1 = list(input().strip())
-s2 = list(input().strip())
+s1=input().strip()
+s2=input().strip()
 
-lcs = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
-for i in range(1, len(s1)+1):
-    for j in range(1, len(s2)+1):
-        if s1[i-1] == s2[j-1]:
-            lcs[i][j] = lcs[i-1][j-1] + 1
+dp=[[0]*(len(s2)+1) for _ in range(len(s1)+1)]
+
+for i in range(1,len(s1)+1):
+    for j in range(1,len(s2)+1):
+        if s1[i-1]==s2[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
         else:
-            lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
-print(max(map(max,lcs)))
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+
+print(dp[-1][-1])
