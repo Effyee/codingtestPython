3da3dc53387b8a8e439b978994f7b7a032f4f757
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-06-27 10:18:00 +0900

    [Gold V] Title: 평범한 배낭, Time: 4224 ms, Memory: 230376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,16 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-things = [list(map(int, input().split())) for _ in range(n)]
+items = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (k+1) for _ in range(n+1)]
 
-dp = [[0] * (k + 1) for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    w, v = things[i - 1]
-    for j in range(k + 1):
-        if j >= w:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+for i in range(1, n+1):
+    weight, value = items[i-1]  # 무게, 가치
+    for j in range(1, k+1):
+        if weight <= j:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j - weight] + value)
         else:
-            dp[i][j] = dp[i - 1][j]
+            dp[i][j] = dp[i-1][j]
 
 print(dp[n][k])
