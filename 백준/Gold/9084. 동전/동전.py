3cf28854d9984d3a5f0b6916ba327fab6abe9c93
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-06-27 13:31:45 +0900

    [Gold V] Title: 동전, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,11 +1,17 @@
 import sys
-T = int(sys.stdin.readline())
+input=sys.stdin.readline
+
+T=int(input())
+
 for _ in range(T):
-    N, coins, M = int(sys.stdin.readline()), list(map(int, sys.stdin.readline().split())), int(sys.stdin.readline())
-    dp = [0]*(M+1)
-    dp[0] = 1 # 0원으로 만드는 가지 수 1개
-    for coin in coins:
-        for money in range(M+1):
-            if money >= coin: # 금액이 동전의 가치보다 크면
-                dp[money] += dp[money-coin] # 해당 dp는 금액 - 동전에 해당하는 dp합
-    print(dp[M])
+    # 동전의 가지 수
+    N=int(input())
+    moneys=list(map(int,input().split()))
+    # 목표 금액
+    m=int(input())
+    dp=[0]*(m+1)
+    dp[0]=1
+    for money in moneys:
+        for i in range(money,m+1):
+            dp[i]+=dp[i-money]
+    print(dp[-1])
