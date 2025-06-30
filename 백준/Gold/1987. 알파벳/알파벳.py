ce00f54720f03247c0cd74eecfaab35ab2f6eda9
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-06-30 13:53:40 +0900

    [Gold IV] Title: 알파벳, Time: 5536 ms, Memory: 165252 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,26 +1,27 @@
 import sys
 input=sys.stdin.readline
-R,C=map(int,input().split())
 
-graph=[]
-for _ in range(R):
-    graph.append(list(map(str,input().strip())))
+r,c=map(int,input().split())
+graph=[list(input().rstrip()) for _ in range(r)]
 
-max_length=0
+visited=[False]*(26)
+answer=0
 
-def dfs(x,y,route):
-    global max_length
-    max_length=max(len(route),max_length)
+def dfs(x,y,cnt):
+    global answer
+    answer=max(answer,cnt)
     dx=[0,0,-1,1]
     dy=[-1,1,0,0]
     for i in range(4):
         nx=x+dx[i]
         ny=y+dy[i]
-        if 0<=nx<R and 0<=ny<C:
-            if graph[nx][ny] not in route:
-                dfs(nx,ny,route+graph[nx][ny])
+        if 0<=nx<r and 0<=ny<c:
+            idx=ord(graph[nx][ny])-ord('A')
+            if not visited[idx]:
+                visited[idx] = True
+                dfs(nx, ny, cnt + 1)
+                visited[idx] = False
 
-route=graph[0][0]
-dfs(0,0,route)
-
-print(max_length)
+visited[ord(graph[0][0])-ord('A')]=True
+dfs(0,0,1)
+print(answer)
