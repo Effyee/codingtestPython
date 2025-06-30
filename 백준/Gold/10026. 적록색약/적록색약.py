43cfc9539bf465200ab8d7518f125fa0bfeae558
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-06-30 14:28:29 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 35084 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -2,62 +2,66 @@ import sys
 from collections import deque
 input=sys.stdin.readline
 
-N=int(input())
-graph=[]
-for _ in range(N):
-    graph.append(list(map(str,input().strip())))
-
-visited=[[False]*len(graph[0]) for _ in range(N)]
+n=int(input())
+graph=[list(map(str,input().rstrip())) for _ in range(n)]
+visited=[[False]*(n) for _ in range(n)]
 
 def bfs(x,y):
-    q=deque([(x,y,graph[x][y])])
+    dx=[0,0,-1,1]
+    dy=[-1,1,0,0]
+    q=deque([[x,y]])
+    visited[x][y] = True
     while q:
-        x,y,color=q.popleft()
-        dx=[0,0,-1,1]
-        dy=[-1,1,0,0]
+        x,y=q.popleft()
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
-            if 0<=nx<N and 0<=ny<len(graph[0]):
-                if not visited[nx][ny] and graph[nx][ny]==color:
-                    visited[nx][ny]=True
-                    q.append((nx,ny,color))
-    return
-
-def grbfs(x,y):
-    q=deque([(x,y,graph[x][y])])
-    while q:
-        x,y,color=q.popleft()
-        dx=[0,0,-1,1]
-        dy=[-1,1,0,0]
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if 0<=nx<N and 0<=ny<len(graph[0]):
-                if color=='R' or color=='G':
-                    if not visited[nx][ny] and graph[nx][ny] in ['R','G']:
+            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+                if graph[x][y]=='R':
+                    if graph[nx][ny]=='R' or graph[nx][ny]=='G':
                         visited[nx][ny] = True
-                        q.append((nx, ny, color))
+                        q.append([nx,ny])
+                elif graph[x][y]=='G':
+                    if graph[nx][ny]=='R' or graph[nx][ny]=='G':
+                        visited[nx][ny] = True
+                        q.append([nx,ny])
                 else:
-                    if not visited[nx][ny] and graph[nx][ny]==color:
-                        visited[nx][ny]=True
-                        q.append((nx,ny,color))
+                    if graph[nx][ny]=='B':
+                        visited[nx][ny] = True
+                        q.append([nx,ny])
+    return 1
+
+answer=0
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs(i,j)
+            answer+=1
+
+visited=[[False]*(n) for _ in range(n)]
+
+def bfs_RGB(x,y):
+    q=deque([[x,y]])
+    visited[x][y] = True
+    while q:
+        x,y=q.popleft()
+        dx=[0,0,-1,1]
+        dy=[-1,1,0,0]
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+                if graph[x][y]==graph[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append([nx,ny])
+
     return
 
-ans1,ans2=0,0
+answer2=0
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs_RGB(i,j)
+            answer2+=1
 
-for x in range(N):
-    for y in range(len(graph[0])):
-        if not visited[x][y]:
-            bfs(x,y)
-            ans1+=1
-
-visited=[[False]*len(graph[0]) for _ in range(N)]
-
-for x in range(N):
-    for y in range(len(graph[0])):
-        if not visited[x][y]:
-            grbfs(x,y)
-            ans2+=1
-
-print(ans1,ans2)
+print(answer2, answer)
