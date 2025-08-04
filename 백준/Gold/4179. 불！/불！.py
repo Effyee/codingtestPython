4e9ed25ccc28e1dc6de5060a0865ecc25781af87
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-08-04 15:41:09 +0900

    [Gold III] Title: 불!, Time: 1148 ms, Memory: 40332 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -2,49 +2,45 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = [list(input().strip()) for _ in range(n)]
-
-ans = "IMPOSSIBLE"
+r, c = map(int, input().split())
+graph = [list(input().rstrip()) for _ in range(r)]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
 def bfs():
     q = deque()
-    visited = [[False] * m for _ in range(n)]
 
-    # 불 먼저
-    for i in range(n):
-        for j in range(m):
+    for i in range(r):
+        for j in range(c):
             if graph[i][j] == 'F':
-                q.append([-1, i, j])
-    # 지훈이 나중
-    for i in range(n):
-        for j in range(m):
+                q.append((i, j, 'F', 0))
+
+    for i in range(r):
+        for j in range(c):
             if graph[i][j] == 'J':
-                q.append([0, i, j])
-                visited[i][j] = True
+                if i == 0 or i == r - 1 or j == 0 or j == c - 1:
+                    return 1
+                q.append((i, j, 'J', 1))
 
-    global ans
     while q:
-        time, x, y = q.popleft()
+        x, y, kind, time = q.popleft()
 
-        if time >= 0 and (x == 0 or x == n - 1 or y == 0 or y == m - 1):
-            ans = time + 1
-            return
-
-        dx = [0, 0, -1, 1]
-        dy = [-1, 1, 0, 0]
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m:
-                if time == -1 and graph[nx][ny] in ['.', 'J']:
-                    graph[nx][ny] = 'F'
-                    q.append([-1, nx, ny])
-                elif time >= 0 and graph[nx][ny] == '.' and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    graph[nx][ny] = 'J'
-                    q.append([time + 1, nx, ny])
+            if 0 <= nx < r and 0 <= ny < c:
+                if kind == 'F':
+                    if graph[nx][ny] == '.' or graph[nx][ny] == 'J':
+                        graph[nx][ny] = 'F'
+                        q.append((nx, ny, 'F', time + 1))
+                else:
+                    if graph[nx][ny] == '.':
+                        if nx == 0 or nx == r - 1 or ny == 0 or ny == c - 1:
+                            return time + 1
+                        graph[nx][ny] = 'J'
+                        q.append((nx, ny, 'J', time + 1))
 
-bfs()
-print(ans)
+    return "IMPOSSIBLE"
+
+print(bfs())
