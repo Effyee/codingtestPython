cc98df79ce7deae3353e42d7a7703b321f4baead
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-08-21 11:19:39 +0900

    [level 3] Title: 순위, Time: 133.39 ms, Memory: 9.61 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,23 +1,22 @@
 def solution(n, results):
     answer = 0
-    graph = [[0]*n for _ in range(n)]
-
+    graph=[[0]*(n) for _ in range(n)]
+    
     for result in results:
-        player1, player2 = result
-        graph[player1-1][player2-1] = 1  # player1이 player2를 이김
-        graph[player2-1][player1-1] = -1  # player2가 player1에게 진다
-
+        a,b=result
+        graph[a-1][b-1]=1
+        graph[b-1][a-1]=-1
+    
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                if i == j or graph[i][j] in [1, -1]:
+                if i==j and graph[i][j] in [1,-1]:
                     continue
-                if graph[i][k] == 1 and graph[k][j] == 1:
-                    # i가 k를 이기고 k가 j를 이긴 경우
-                    graph[i][j] = 1  # i가 j를 이김
-                    graph[j][i] = -1  # j가 i에게 짐
-
-    for row in graph:
-        if row.count(0) == 1:
-            answer += 1
+                if graph[i][k]==graph[k][j]==1:
+                    graph[i][j]=1
+                    graph[j][k]=graph[k][i]=graph[j][i]=-1
+    for row in range(n):
+        if graph[row].count(0)==1:
+            answer+=1
+        
     return answer
