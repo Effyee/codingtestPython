56572e2342c31d70f16b8a0fba8f7299ad5799ac
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-08-21 10:35:00 +0900

    [level 3] Title: 가장 먼 노드, Time: 90.74 ms, Memory: 26.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,42 +1,36 @@
 import heapq
-
-INF = int(1e9)
-
-
 def solution(n, edge):
-    answer = 0
-    graph = [[] for _ in range(n + 1)]
+    answer = 1
+    graph=[[] for _ in range(n+1)]
+    for s,e in edge:
+        graph[s].append((e,1))
+        graph[e].append((s,1))
 
-    for start, dest in edge:
-        graph[start].append(dest)  # start에서 dest로 가는 간선 추가
-        graph[dest].append(start)  # dest에서 start로 가는 간선 추가 (양방향)
-
-    distance = dijkstra(n, graph)
-
-    # 최댓값을 찾아 그 노드의 개수를 센다
-    max_distance = max(distance[1:])  # 0번 인덱스는 사용하지 않음
-    answer = distance.count(max_distance)  # 가장 멀리 있는 노드의 개수
-
+    def dijkstra(start):
+        INF=int(1e9)
+        distance=[INF]*(n+1)
+        hq=[]
+        heapq.heappush(hq,(0,start))
+        distance[start]=0
+        while hq:
+            dist,now=heapq.heappop(hq)
+            if distance[now]<dist:
+                continue
+            for nxt,d in graph[now]:
+                cost=d+dist
+                if distance[nxt]>cost:
+                    distance[nxt]=cost
+                    heapq.heappush(hq,(cost,nxt))
+        return distance
+    
+    distance=dijkstra(1)
+    m=-int(1e9)
+    print(distance)
+    for i in range(2,n+1):
+        if distance[i]>m:
+            m=max(m,distance[i])
+            answer=1
+        elif distance[i]==m:
+            answer+=1
+        
     return answer
-
-
-def dijkstra(n, graph):
-    distance = [INF] * (n + 1)
-    distance[1] = 0  # 시작 노드의 거리는 0
-    q = []
-    heapq.heappush(q, (0, 1))  # (거리, 노드)
-
-    while q:
-        dist, now = heapq.heappop(q)
-
-        if distance[now] < dist:
-            continue
-
-        for v in graph[now]:  # 연결된 노드
-            cost = dist + 1  # 모든 간선의 가중치는 1
-            if cost < distance[v]:
-                distance[v] = cost
-                heapq.heappush(q, (cost, v))
-
-    return distance
-
