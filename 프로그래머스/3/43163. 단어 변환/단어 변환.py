0863daa4235cb2153c6d29342c410d33d63700fd
Effy__ee <97147501+Effyee@users.noreply.github.com>
2025-08-22 14:42:47 +0900

    [level 3] Title: 단어 변환, Time: 0.49 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,32 +1,22 @@
 from collections import deque
 
-def check(word1,word2):
-    count=0
-    for i in range(len(word1)):
-        if word1[i]!=word2[i]:
-            count+=1
-    if count==1:
-        return True
-    return False
-
-def bfs(words,visited,begin,target):
-    q=deque()
-    q.append([begin,0])
-
-    while q:
-        now,count=q.popleft()
-        if now==target:
-            return count
-        for i in range(len(words)):
-            if not visited[i]:
-                if check(now,words[i]):
-                    visited[i]=True
-                    q.append((words[i],count+1))
-
-
 def solution(begin, target, words):
     if target not in words:
         return 0
-    visited=[False]*len(words)
-    answer=bfs(words,visited,begin,target)
-    return answer
+    
+    def one_diff(w1, w2):
+        return sum(a!=b for a,b in zip(w1, w2)) == 1
+    
+    q = deque()
+    q.append((begin, 0))
+    visited = [False] * len(words)
+    
+    while q:
+        now, cnt = q.popleft()
+        if now == target:
+            return cnt
+        for i, word in enumerate(words):
+            if not visited[i] and one_diff(now, word):
+                visited[i] = True
+                q.append((word, cnt+1))
+    return 0
