cef2e23e0694a331689cbe52152e523780c70592
Effy__ee <gayean01@gmail.com>
2025-08-27 16:35:49 +0900

    [Gold I] Title: 계단 수, Time: 660 ms, Memory: 40488 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.py
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -1,28 +1,30 @@
-N = int(input())
-MOD = 1e9
-# dp[N번째 수][마지막 수][방문한 수 bitmasking(0~1023)]
-dp = [[[0] * 1024 for _ in range(10)] for _ in range(N + 1)]
+import sys
+input=sys.stdin.readline
+MOD=int(1e9)
+N=int(input())
+# dp[숫자 자리수][마지막 수][비트마스킹 방문여부체크(0~9)]
+dp=[[[0]*1024 for _ in range(10)] for _ in range(N+1)]
 
-for i in range(1, 10):
-    dp[1][i][1 << i] = 1
+# 초기화
+for i in range(1,10):
+    dp[1][i][1<<i]=1
 
-# n = N번째 수
-for n in range(2, N + 1):
-    # i = 마지막 방문 숫자가 i
-    for i in range(10):
-        # 0~9까지 모든 수를 방문해야 한다는 조건이 있으므로, 방문 여부를 bitmasking을 통해 저장해야 함.
+# 자릿수
+for n in range(2,N+1):
+    for i in range(0,10):
         for bit in range(1024):
-            if i == 0:
-                dp[n][i][bit | (1 << i)] += dp[n - 1][i + 1][bit]
-            elif i == 9:
-                dp[n][i][bit | (1 << i)] += dp[n - 1][i - 1][bit]
+            # 마지막 수가 0인 경우 다음 오는 수는 1
+            if i==0:
+                dp[n][i][bit|(1<<i)]+=dp[n-1][i+1][bit]
+            # 마지막 수가 9인 경우 다음 오는 수는 8
+            elif i==9:
+                dp[n][i][bit|(1<<i)]+=dp[n-1][i-1][bit]
             else:
-                dp[n][i][bit | (1 << i)] += dp[n - 1][i - 1][bit] + dp[n - 1][i + 1][bit]
+                dp[n][i][bit|(1<<i)]+=(dp[n-1][i-1][bit]+dp[n-1][i+1][bit])
+            dp[n][i][bit|(1<<i)]%=MOD
 
-            dp[n][i][bit | (1 << i)] %= MOD
-
-res = 0
+res=0
 for i in range(10):
-    res += dp[N][i][2 ** 10 - 1]
+    res+=dp[N][i][2**10-1]
 
-print(int(res % MOD))
+print(int(res%MOD))
