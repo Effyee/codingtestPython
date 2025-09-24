2a299445306397e53206ab024160c0c6983782a1
Effy__ee <gayean01@gmail.com>
2025-09-24 16:12:59 +0900

    [Silver II] Title: 나무 자르기, Time: 2744 ms, Memory: 148224 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -2,23 +2,18 @@ import sys
 input=sys.stdin.readline
 
 n,m=map(int,input().split())
-trees=list(map(int,input().split()))
-trees.sort()
+tree=list(map(int,input().split()))
 
-def bs():
-    answer=0
-    start,end=0,max(trees)
-    while start<=end:
-        mid=(start+end)//2
-        cnt=0
-        for i in range(n):
-            if trees[i]>mid:
-                cnt+=trees[i]-mid
-        if cnt>=m:
-            answer=mid
-            start=mid+1
-        else:
-            end=mid-1
-    return answer
-
-print(bs())
+answer=0
+start,end=1,max(tree)
+while start<=end:
+    mid=(start+end)//2
+    result = sum(max(0, t - mid) for t in tree)
+    # m 미터 이상의 나무가 확보된 경우, 답에 넣고
+    # 높이를 늘린다
+    if result>=m:
+        answer=mid
+        start=mid+1
+    else:
+        end=mid-1
+print(answer)
