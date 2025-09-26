1f5b77b2f610884a317d34dc11fe591bd4504571
Effy__ee <gayean01@gmail.com>
2025-09-26 13:56:18 +0900

    [Gold III] Title: 소수의 연속합, Time: 724 ms, Memory: 75136 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,34 +1,31 @@
 import sys
 input=sys.stdin.readline
 
-# 연속된 소수의 합
 n=int(input())
-
 def find_prime(n):
-    primes=[True]*(n+1)
-    primes[0]=False
-    primes[1]=False
-    for i in range(2,int(n**(1/2))+1):
-        if primes[i]:
+    isthisprime=[True]*(n+1)
+    isthisprime[0]=False
+    isthisprime[1]=False
+    for i in range(2,int(n**0.5)+1):
+        if isthisprime[i]:
             for j in range(i*2,n+1,i):
-                primes[j]=False
-    return [i for i in range(2,n+1) if primes[i]==True]
+                isthisprime[j]=False
+    return [i for i in range(1,n+1) if isthisprime[i]]
 
 primes=find_prime(n)
-
-left,right,total=0,0,0
-answer=0
+left,right,result,answer=0,0,0,0
 while right<len(primes):
-    if total<n:
-        total+=primes[right]
+    if result<n:
+        result+=primes[right]
         right+=1
-    if total>n:
+    if result>n:
         if left<right:
-            total-=primes[left]
+            result -= primes[left]
             left+=1
-    if total==n:
+    if result==n:
         answer+=1
-        if left<right:
-            total-=primes[left]
-            left+=1
+        if left < right:
+            result -= primes[left]
+            left += 1
+    
 print(answer)
