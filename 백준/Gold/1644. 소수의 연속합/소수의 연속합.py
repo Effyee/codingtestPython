84303af3f82af2211f1a57d845c0a09314921db4
Effy__ee <gayean01@gmail.com>
2025-09-26 13:44:34 +0900

    [Gold III] Title: 소수의 연속합, Time: 828 ms, Memory: 75136 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,40 +1,34 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-n = int(input())
-primes = []
+# 연속된 소수의 합
+n=int(input())
 
-def findprime(n):
-    global primes
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
+def find_prime(n):
+    primes=[True]*(n+1)
+    primes[0]=False
+    primes[1]=False
+    for i in range(2,int(n**(1/2))+1):
+        if primes[i]:
+            for j in range(i*2,n+1,i):
+                primes[j]=False
+    return [i for i in range(2,n+1) if primes[i]==True]
 
-    for i in range(2, int(n ** 0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, n + 1, i):
-                is_prime[j] = False
+primes=find_prime(n)
 
-    primes = [i for i, val in enumerate(is_prime) if val]
-
-findprime(n)
-
-start, end = 0, 0
-total = 0
-count = 0
-
-while end <= len(primes):
-    if total < n:
-        if end < len(primes):
-            total += primes[end]
-        end += 1
-    elif total > n:
-        if start < len(primes):
-            total -= primes[start]
-        start += 1
-    else:
-        count += 1
-        if end < len(primes):
-            total += primes[end]
-        end += 1
-
-print(count)
+left,right,total=0,0,0
+answer=0
+while right<len(primes):
+    if total<n:
+        total+=primes[right]
+        right+=1
+    if total>n:
+        if left<right:
+            total-=primes[left]
+            left+=1
+    if total==n:
+        answer+=1
+        if left<right:
+            total-=primes[left]
+            left+=1
+print(answer)
