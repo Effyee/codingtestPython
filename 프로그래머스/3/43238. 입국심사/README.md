9d8a56e90e448a860d0e71306cf5ee366e809558
Effy__ee <gayean01@gmail.com>
2025-10-02 13:19:06 +0900

    [level 3] Title: 입국심사, Time: 812.09 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#) 
 
 ### 성능 요약
 
-메모리: 13.3 MB, 시간: 660.70 ms
+메모리: 13.2 MB, 시간: 812.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 21일 13:53:28
+2025년 10월 02일 13:19:03
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,20 +1,15 @@
 def solution(n, times):
     answer = 0
-    # n: 기다리는 사람 수, times: 한 명을 심사하는 데 걸리는 시간
     times.sort()
-    start,end=times[0],times[-1]*n
-    
+    start,end=1,times[-1]*n
     while start<=end:
         mid=(start+end)//2
-        print(f"start:{start}, end:{end}, mid:{mid}")
-        re=0
+        count=0
         for time in times:
-            re+=mid//time
-        print(re)
-        if re>=n:
+            count+=mid//time
+        if count>=n:
             answer=mid
             end=mid-1
         else:
             start=mid+1
-            
     return answer
