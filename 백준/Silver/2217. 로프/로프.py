4c9ec2dfd125249357afc696982c0dfddd99dd0b
Effy__ee <gayean01@gmail.com>
2025-10-05 11:14:36 +0900

    [Silver IV] Title: 로프, Time: 100 ms, Memory: 37048 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -2,10 +2,12 @@ import sys
 input=sys.stdin.readline
 
 n=int(input())
-li=sorted([int(input()) for _ in range(n)],reverse=True)
-answer=-int(1e9)
+# k개의 로프를 사용해서 중량이 w인 물체를 들어올린다면,
+# w/k 만큼 걸린다.
 
+ropes=[int(input()) for _ in range(n)]
+ropes=sorted(ropes,reverse=True)
+answer=0
 for i in range(n):
-    answer=max(answer,li[i]*(i+1))
-
+    answer=max(answer,(i+1)*ropes[i])
 print(answer)
