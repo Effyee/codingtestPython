4c65c47d4f2ec63f8552435732228d55d8e3c8c6
Effy__ee <gayean01@gmail.com>
2025-10-08 16:04:25 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,27 +1,27 @@
-def find_parent(x,parent):
-    if parent[x]!=x:
-        parent[x]=find_parent(parent[x],parent)
-    return parent[x]
-
-def make_union(a,b,parent):
-    a=find_parent(a,parent)
-    b=find_parent(b,parent)
-
-    if a<b:
-        parent[b]=a
-    else:
-        parent[a]=b
-
-    return parent
-
+import sys
+input=sys.stdin.readline
+sys.setrecursionlimit(int(1e6))
 def solution(n, costs):
     answer = 0
-    costs=sorted(costs,key=lambda x:x[2])
+    costs=sorted(costs,key= lambda x:x[2])
     parent=[i for i in range(n)]
-    for cost in costs:
-        a,b,c=cost
-        if find_parent(a,parent)!=find_parent(b,parent):
-            make_union(a,b,parent)
-            answer+=c
-
+    def find_parent(x,parent):
+        if parent[x]!=x:
+            parent[x]=find_parent(parent[x],parent)
+        return parent[x]
+    
+    def make_union(a,b,parent):
+        a=find_parent(a,parent)
+        b=find_parent(b,parent)
+        if a<=b:
+            parent[b]=a
+        else:
+            parent[a]=b
+    
+    for start,end,cost in costs:
+        if find_parent(start,parent)!=find_parent(end,parent):
+            answer+=cost
+            make_union(start,end,parent)
+        
+            
     return answer
