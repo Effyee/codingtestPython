c32577f481c61728a11693d8116af0b0d32b6fbc
Effy__ee <gayean01@gmail.com>
2025-10-08 16:22:42 +0900

    [level 3] Title: 단속카메라, Time: 4.69 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,9 +1,15 @@
 def solution(routes):
     answer = 0
-    camera=-30001
-    routes.sort(key=lambda x: x[1])
-    for route in routes:
-        if camera<route[0]:
-            answer+=1
-            camera=route[1]
-    return answer
+    routes=sorted(routes,key=lambda x:x[0])
+    start,end=routes[0]
+    new=[]
+    for i in range(1,len(routes)):
+        if start<=routes[i][0]<=end:
+            start=max(start,routes[i][0])
+            end=min(end,routes[i][1])
+            print(start,end)
+        else:
+            new.append([start,end])
+            start,end=routes[i]
+
+    return len(new)+1
