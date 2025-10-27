41337e230dd97c54c1a025f2c5c6ada41e4c3d5c
Effy__ee <gayean01@gmail.com>
2025-10-27 10:02:18 +0900

    [Gold III] Title: 소수의 연속합, Time: 708 ms, Memory: 75136 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -2,30 +2,30 @@ import sys
 input=sys.stdin.readline
 
 n=int(input())
-def find_prime(n):
-    isthisprime=[True]*(n+1)
-    isthisprime[0]=False
-    isthisprime[1]=False
-    for i in range(2,int(n**0.5)+1):
-        if isthisprime[i]:
-            for j in range(i*2,n+1,i):
-                isthisprime[j]=False
-    return [i for i in range(1,n+1) if isthisprime[i]]
 
-primes=find_prime(n)
-left,right,result,answer=0,0,0,0
-while right<len(primes):
-    if result<n:
-        result+=primes[right]
+def is_prime(n):
+    isthisprime = [True] * (n + 1)
+    isthisprime[0] = False
+    isthisprime[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if isthisprime[i]:
+            for j in range(i * 2, n + 1, i):
+                isthisprime[j] = False
+    return [i for i in range(2,n+1) if isthisprime[i]]
+
+arr=is_prime(n)
+l=len(arr)
+left,right,re=0,0,0
+answer=0
+while left<l and right<l:
+    if re<n:
+        re+=arr[right]
         right+=1
-    if result>n:
-        if left<right:
-            result -= primes[left]
-            left+=1
-    if result==n:
+    if re>n:
+        re-=arr[left]
+        left+=1
+    if re==n:
         answer+=1
-        if left < right:
-            result -= primes[left]
-            left += 1
-    
+        re -= arr[left]
+        left += 1
 print(answer)
