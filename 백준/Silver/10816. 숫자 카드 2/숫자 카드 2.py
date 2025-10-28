90af9ec7a81154b8809131fe853c71e5e99f45b0
Effy__ee <gayean01@gmail.com>
2025-10-28 14:57:26 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 3136 ms, Memory: 115484 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,51 +1,33 @@
 import sys
+input=sys.stdin.readline
 
-input = sys.stdin.readline
-
-n = int(input())
-arr = list(map(int, input().split()))
+n=int(input())
+arr=list(map(int,input().split()))
 arr.sort()
-m = int(input())
-nums = list(map(int, input().split()))
+m=int(input())
+targets=list(map(int,input().split()))
 
-
-# target 이상인 인덱스 중 가장 작은 (없으면 -1)
 def lower_bound(target):
-    answer = -1
-    start, end = 0, n - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if arr[mid] >= target:
-            answer = mid
-            end = mid - 1
+    start,end=0,len(arr)
+    while start<end:
+        mid=(start+end)//2
+        if arr[mid]<target:
+            start=mid+1
         else:
-            start = mid + 1
-    return answer
+            end=mid
+    return start
 
-# target 초과인 인덱스 중 가장 작은 (없으면 -1)
 def upper_bound(target):
-    answer = -1
-    start, end = 0, n - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if arr[mid] > target:
-            answer = mid
-            end = mid - 1
+    start,end=0,len(arr)
+    while start<end:
+        mid=(start+end)//2
+        if arr[mid]<=target:
+            start=mid+1
         else:
-            start = mid + 1
-    return answer
+            end=mid
+    return start
+answer=[]
+for i in range(m):
+    answer+=[upper_bound(targets[i])-lower_bound(targets[i])]
 
-result = []
-for num in nums:
-    low = lower_bound(num)
-    up = upper_bound(num)
-
-    # 없는 경우
-    if low == -1:
-        result.append(0)
-    else:
-        if up == -1:  # num보다 큰 값이 없으면 → 끝까지 다 num임
-            up = n
-        result.append(up - low)
-
-print(" ".join(map(str, result)))
+print(*answer)
