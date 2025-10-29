dff68b5cb5b127cb612300a5bd87e5b9f5a8989a
Effy__ee <gayean01@gmail.com>
2025-10-29 14:47:43 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -1,42 +1,31 @@
 import sys
 input=sys.stdin.readline
 
-N, K = map(int, input().split())
-order = list(map(int, input().split()))
-
-plug = []  # 현재 콘센트에 꽂혀 있는 기기들
-count = 0  # 뽑은 횟수
-
-for i in range(K):
-    curr = order[i]
-
-    # 이미 꽂혀 있으면 패스
-    if curr in plug:
+n,k=map(int,input().split())
+arr=list(map(int,input().split()))
+consent=[]
+answer=0
+for i in range(k):
+    if arr[i] in consent:
+        continue
+    if len(consent)<n:
+        consent.append(arr[i])
         continue
 
-    # 아직 자리 남아 있으면 꽂기
-    if len(plug) < N:
-        plug.append(curr)
-        continue
+    target=-1
+    farthest=-1
 
-    # 자리가 없으면, 앞으로 가장 늦게 쓰일 기기를 뺀다
-    farthest = -1
-    target = -1
-
-    for p in plug:
-        # 앞으로 또 사용되는지 확인
-        if p in order[i+1:]:
-            next_use = order[i+1:].index(p)
+    for plugged in consent:
+        if plugged not in arr[i+1:]:
+            target=plugged
+            break
         else:
-            next_use = K  # 다시 안 쓰이면 제일 먼 시점으로
+            idx=arr[i+1:].index(plugged)
+            if farthest<idx:
+                farthest=idx
+                target=plugged
+    consent.remove(target)
+    consent.append(arr[i])
+    answer+=1
 
-        # 가장 나중에 쓰이는 기기 찾기
-        if next_use > farthest:
-            farthest = next_use
-            target = p
-
-    plug.remove(target)
-    plug.append(curr)
-    count += 1
-
-print(count)
+print(answer)
