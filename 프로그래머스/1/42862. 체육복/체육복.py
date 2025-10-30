a49715868c18ed2ecb7424733718b8f45413cd47
Effy__ee <gayean01@gmail.com>
2025-10-30 10:09:43 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,17 @@
 def solution(n, lost, reserve):
-    real_lost = list(set(lost) - set(reserve))
-    real_reserve = list(set(reserve) - set(lost))
-    
-    answer = n - len(real_lost)  # 처음 체육복 있는 학생 수
-    
-    for l in sorted(real_lost):
-        if l-1 in real_reserve:
-            real_reserve.remove(l-1)
+    # 서로 겹치는 학생(자기 옷을 잃었지만 여분도 가진 경우) 제거
+    new_r = sorted(set(reserve) - set(lost))
+    new_l = set(lost) - set(reserve)
+
+    assigned = set()   # 이미 체육복을 받은 학생(중복 수여 방지)
+    answer = n - len(new_l)  # 기본으로 옷 있는 학생 수 (reserve 겹친 경우 제거 후)
+
+    for r in new_r:
+        if r-1 in new_l and (r-1) not in assigned:
+            assigned.add(r-1)
             answer += 1
-        elif l+1 in real_reserve:
-            real_reserve.remove(l+1)
+        elif r+1 in new_l and (r+1) not in assigned:
+            assigned.add(r+1)
             answer += 1
-    
+
     return answer
