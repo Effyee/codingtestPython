2a4959bcb277d848f3cc0a3a1312cd1f394139a7
Effy__ee <gayean01@gmail.com>
2025-12-09 12:23:28 +0900

    [level 2] Title: 완전범죄, Time: 0.84 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,30 +1,22 @@
-def dfs(a, b, idx, info, n, m, dp):
-    global rs
-    if idx == len(info):
-        if a < n and b < m:
-            rs = min(rs, a)
-        return
-    
-    if (a, b, idx) not in dp:
-        dp.add((a, b, idx))
-    else:
-        return
-    
-    if a >= n or b >= m or a >= rs:
-        return
-    
-    dfs(a+info[idx][0], b, idx+1, info, n, m, dp)
-    dfs(a, b+info[idx][1], idx+1, info, n, m, dp)
-    
-    
 def solution(info, n, m):
-    global rs
-    rs = float('inf')
-    
-    dp = set()
-    dfs(0, 0, 0, info, n, m, dp)
-    
-    if rs == float('inf'):
+    answer = 0
+    INF=int(1e9)
+    dp=[INF]*m
+    dp[0]=0
+    for a_trace,b_trace in info:
+        new_dp=[INF]*(m)
+        for prev_b in range(m):
+            if dp[prev_b]==INF:
+                continue
+            # A가 훔치는 경우
+            a_new=a_trace+dp[prev_b]
+            if a_new<n:
+                new_dp[prev_b]=min(a_new,new_dp[prev_b])
+            # B가 훔치는 경우
+            b_new=b_trace+prev_b
+            if b_new<m:
+                new_dp[b_new]=min(dp[prev_b],new_dp[b_new])
+        dp=new_dp
+    if min(dp)==INF:
         return -1
-    else:
-        return rs
+    return min(dp)
