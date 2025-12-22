bbd76482466efe7b5b9a7030572ac8f22f2c01f9
Effy__ee <gayean01@gmail.com>
2025-12-22 12:38:59 +0900

    [level 2] Title: 완전범죄, Time: 1.08 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,26 +1,20 @@
-INF = int(1e9)
-
 def solution(info, n, m):
-    # arr[b] = B 흔적이 b일 때 가능한 A 흔적 최소값
-    arr = [INF] * m
-    arr[0] = 0
-
-    for A, B in info:
-        new_arr = [INF] * m  # 반드시 새로 만들어야 함
-
+    answer = 0
+    INF=int(1e9)
+    arr=[INF]*m
+    arr[0]=0
+    for A,B in info:
+        new_arr=[INF]*m
         for b in range(m):
-            if arr[b] == INF:
+            if arr[b]==INF:
                 continue
-
-            # A가 훔치는 경우
-            if arr[b] + A < n:
-                new_arr[b] = min(new_arr[b], arr[b] + A)
-
-            # B가 훔치는 경우
-            if b + B < m:
-                new_arr[b + B] = min(new_arr[b + B], arr[b])
-
-        arr = new_arr
-
-    res = min(arr)
-    return res if res < INF else -1
+            # A가 훔침
+            if arr[b]+A<n:
+                new_arr[b]=min(new_arr[b],arr[b]+A)
+            # B가 훔침
+            if b+B<m:
+                new_arr[b+B]=min(arr[b],new_arr[b+B])
+        arr=new_arr
+        
+    res=min(arr)
+    return res if res<INF else -1
