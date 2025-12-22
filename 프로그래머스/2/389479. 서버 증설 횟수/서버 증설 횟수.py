a60f17b2c861bb30340705ff7b38f83387cc503f
Effy__ee <gayean01@gmail.com>
2025-12-22 13:44:00 +0900

    [level 2] Title: 서버 증설 횟수, Time: 1.76 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -1,12 +1,22 @@
 def solution(players, m, k):
-    server = [0] * len(players)
-    cnt = 0
-    
-    for i in range(len(players)):
-        remain = players[i]-(server[i]*m)
-        if remain >= m:
-            end = min(i+k, len(players))
-            for j in range(i, end):
-                server[j] += remain // m
-            cnt += remain // m
-    return cnt
+    answer = 0
+    # 현재 서버 대수: n
+    n=0
+    s=[]
+    for player in players:
+        if n>0:
+            for i in range(len(s)-1,-1,-1):
+                s[i]-=1
+                if s[i]==0:
+                    s.pop(i)
+                    n-=1
+            
+        if player>=(n+1)*m:
+            needed_server=player//m
+            added_server=needed_server-n
+            n+=added_server
+            answer+=added_server
+            s+=[k]*added_server
+        
+        
+    return answer
