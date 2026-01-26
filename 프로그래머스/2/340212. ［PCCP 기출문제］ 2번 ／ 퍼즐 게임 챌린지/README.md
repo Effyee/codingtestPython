d06b2abeca37a43e455bfec22f5baccd259525b0
Effy__ee <gayean01@gmail.com>
2026-01-26 16:38:33 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 2104.38 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212) 
 
 ### 성능 요약
 
-메모리: 34.1 MB, 시간: 1489.22 ms
+메모리: 34.2 MB, 시간: 2104.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 02일 11:28:23
+2026년 01월 26일 16:38:32
 
 ### 문제 설명
 
--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,28 +1,26 @@
+# 이분 탐색
 def solution(diffs, times, limit):
-    def calculate(level):
-        t=0
+    answer = 0
+    def count_time(level):
+        time,time_prev=0,0
         for i in range(len(diffs)):
             if diffs[i]<=level:
-                t+=times[i]
-            else:
-                retry=diffs[i]-level
-                if i>=1:
-                    t+=(times[i-1]+times[i])*retry+times[i]
-                else:
-                    t+=times[i]
-            if t > limit:  
-                return t
-        return t
+                time+=times[i]
+                time_prev=times[i]
+            elif diffs[i]>level:
+                t=diffs[i]-level
+                time+=(times[i]+time_prev)*t
+                time+=times[i]
+                time_prev=times[i]
+        return time
     
-    answer = 0
     start,end=1,max(diffs)
     while start<=end:
         mid=(start+end)//2
-        ans=calculate(mid)
-        if ans<=limit:
+        t=count_time(mid)
+        if t<=limit:
             answer=mid
             end=mid-1
         else:
             start=mid+1
-    
     return answer
