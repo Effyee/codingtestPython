b4f32781e28b61d6f582839a6545eba9df810b57
Effy__ee <gayean01@gmail.com>
2026-01-28 14:31:56 +0900

    [level 2] Title: 비밀 코드 해독, Time: 259.91 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,33 +1,25 @@
 def solution(n, q, ans):
     answer = 0
-    nums = [True] * (n + 1)
-
-    # ans == 0 인 숫자 제거
-    for i in range(len(q)):
-        if ans[i] == 0:
-            for x in q[i]:
-                nums[x] = False
-
     def check(li):
-        li_set = set(li)
         for i in range(len(q)):
-            if len(set(q[i]) & li_set) != ans[i]:
+            if len(set(li)&set(q[i]))!=ans[i]:
                 return False
         return True
-
-    def backtrack(start, li):
+    
+    def backtrack(idx,li):
         nonlocal answer
-
-        if len(li) == 5:
+        if len(li)==5:
             if check(li):
-                answer += 1
+                answer+=1
             return
-
-        for num in range(start, n + 1):
-            if nums[num]:
-                li.append(num)
-                backtrack(num + 1, li)
-                li.pop()
-
-    backtrack(1, [])
+        if idx==n+1:
+            return
+        for i in range(idx,n+1):
+            li.append(i)
+            backtrack(i+1,li)
+            li.pop()
+        return
+    
+    backtrack(1,[])
+    
     return answer
