223b3ea1b960499e146016607b8969d25adee5f1
Effy__ee <gayean01@gmail.com>
2026-02-12 15:31:50 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.37 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,41 +1,49 @@
 def solution(numbers, hand):
     answer = ''
-    key_dict = {1:(0,0), 2:(0,1), 3:(0,2),
-                4:(1,0), 5:(1,1), 6:(1,2),
-                7:(2,0), 8:(2,1), 9:(2,2),
-                '*':(3,0), 0:(3,1), '#':(3,2)}
     
-    left = [1, 4, 7]
-    right = [3, 6, 9]
-    lhand = '*'
-    rhand = '#'
+    # 키패드 좌표 매핑
+    keypad = {
+        1:(0,0), 2:(0,1), 3:(0,2),
+        4:(1,0), 5:(1,1), 6:(1,2),
+        7:(2,0), 8:(2,1), 9:(2,2),
+        '*':(3,0), 0:(3,1), '#':(3,2)
+    }
+    
+    # 시작 위치
+    left = keypad['*']
+    right = keypad['#']
     
     for num in numbers:
-        if num in left:
+        # 왼쪽 열
+        if num in [1,4,7]:
             answer += 'L'
-            lhand = num
-        elif num in right:
+            left = keypad[num]
+        
+        # 오른쪽 열
+        elif num in [3,6,9]:
             answer += 'R'
-            rhand = num
+            right = keypad[num]
+        
+        # 가운데 열
         else:
-            curPos = key_dict[num]
-            lPos = key_dict[lhand]
-            rPos = key_dict[rhand]
+            target = keypad[num]
             
-            ldist = abs(curPos[0]-lPos[0]) + abs(curPos[1]-lPos[1])
-            rdist = abs(curPos[0]-rPos[0]) + abs(curPos[1]-rPos[1])
+            # 맨해튼 거리 계산
+            left_dist = abs(left[0] - target[0]) + abs(left[1] - target[1])
+            right_dist = abs(right[0] - target[0]) + abs(right[1] - target[1])
             
-            if ldist < rdist:
+            if left_dist < right_dist:
                 answer += 'L'
-                lhand = num
-            elif ldist > rdist:
+                left = target
+            elif right_dist < left_dist:
                 answer += 'R'
-                rhand = num
+                right = target
             else:
-                if hand == "left":
-                    answer += 'L'
-                    lhand = num
-                else:
+                if hand == "right":
                     answer += 'R'
-                    rhand = num
+                    right = target
+                else:
+                    answer += 'L'
+                    left = target
+    
     return answer
