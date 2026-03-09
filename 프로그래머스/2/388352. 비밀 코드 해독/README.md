0775c6c7c4506a59a15723c9a540d0bba669656a
Effy__ee <gayean01@gmail.com>
2026-03-09 17:34:09 +0900

    [level 2] Title: 비밀 코드 해독, Time: 203.69 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/README.md
+++ 프로그래머스/2/388352. 비밀 코드 해독/README.md
@@ -1,10 +1,10 @@
 # [level 2] 비밀 코드 해독 - 388352 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388352?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388352) 
 
 ### 성능 요약
 
-메모리: 9.26 MB, 시간: 176.75 ms
+메모리: 9.2 MB, 시간: 203.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 08일 13:35:25
+2026년 03월 09일 17:34:04
 
 ### 문제 설명
 
--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,19 +1,20 @@
 from itertools import combinations
 def solution(n, q, ans):
     answer = 0
-    cans=[i for i in range(1,n+1)]
-    for i in range(len(q)):
+    nums=[i for i in range(1,n+1)]
+    for i in range(len(ans)):
         if ans[i]==0:
-            for j in q[i]:
-                if j in cans:
-                    cans.remove(j)
-                
-    for c in combinations(cans,5):
+            for num in q[i]:
+                if num in nums:
+                    nums.remove(num)
+                    
+    for c in combinations(nums,5):
         flag=True
-        for i in range(len(q)):
-            if len(set(c) & set(q[i]))!=ans[i]:
+        for i in range(len(ans)):
+            if len(set(c)&set(q[i]))!=ans[i]:
                 flag=False
                 break
         if flag:
             answer+=1
+        
     return answer
