f3f719a14f70f8d6881800694994358916009363
Effy__ee <gayean01@gmail.com>
2026-03-09 18:12:45 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,25 +1,28 @@
 def solution(storey):
-    answer = []
-
-    # st: 현재 층, cnt: 지금까지 사용한 돌 개수
-    def dfs(st, cnt):
-        # 0층이면 끝
-        if st == 0:
-            answer.append(cnt)
+    answer = int(1e9)
+    
+    def bfs(idx, floor):
+        nonlocal answer
+        
+        if idx >= answer:
             return
-
-        one = st % 10        # 일의 자리
-        up = 10 - one        # 올려서 10으로 만드는 비용
-        down = one           # 내려서 0으로 만드는 비용
-
-        # 올리는 쪽이 더 싸면 올리기만
-        if up < down:
-            dfs(st // 10 + 1, cnt + up)
+        
+        if floor == 0:
+            answer = min(answer, idx)
+            return
+        
+        down = floor % 10
+        up = 10 - down
+        
+        if down == 0:
+            bfs(idx, floor // 10)
         elif down < up:
-            dfs(st // 10, cnt + down)
+            bfs(idx + down, floor - down)
+        elif down > up:
+            bfs(idx + up, floor + up)
         else:
-            dfs(st // 10 + 1, cnt + up)   
-            dfs(st // 10, cnt + down)     
+            bfs(idx + down, floor - down)
+            bfs(idx + up, floor + up)
 
-    dfs(storey, 0)
-    return min(answer)
+    bfs(0, storey)
+    return answer
