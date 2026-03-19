f355fc96e39a40af1c69d654b7bc75dcb7de63cb
Effy__ee <gayean01@gmail.com>
2026-03-19 14:17:03 +0900

    [Gold II] Title: 할로윈의 양아치, Time: 1668 ms, Memory: 809592 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
@@ -1,47 +1,48 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-# 입력
-n, m, k = map(int, input().split())
-candy = [0] + list(map(int, input().split()))
+n,m,k=map(int,input().split())
+candy=list(map(int,input().split()))
+parent=[i for i in range(n+1)]
 
-parent = list(range(n+1))
-
-# 유니온 파인드
-def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
+def find_parent(x,parent):
+    if parent[x]!=x:
+        parent[x]=find_parent(parent[x],parent)
     return parent[x]
 
-def union(a, b):
-    pa, pb = find(a), find(b)
-    if pa != pb:
-        parent[pb] = pa
+def make_union(a,b,parent):
+    a=find_parent(a,parent)
+    b=find_parent(b,parent)
+    if a<=b:
+        parent[b]=a
+    else:
+        parent[a]=b
 
-# 친구 관계 묶기
 for _ in range(m):
-    a, b = map(int, input().split())
-    union(a, b)
+    a,b=map(int,input().split())
+    make_union(a,b,parent)
 
-# 그룹별 인원 수, 사탕 합 구하기
-group_size = [0]*(n+1)
-group_candy = [0]*(n+1)
+group_candy=[0]*(n+1)
+group_size=[0]*(n+1)
 
 for i in range(1, n+1):
-    root = find(i)
+    root = find_parent(i,parent)
     group_size[root] += 1
-    group_candy[root] += candy[i]
+    group_candy[root] += candy[i-1]
 
-#그룹 리스트
 groups = []
 for i in range(1, n+1):
     if group_size[i] > 0:
         groups.append((group_size[i], group_candy[i]))
 
-dp = [0]*k  # k명 이상이면 안되니까 k-1까지만
+dp = [[0]*k for _ in range(len(groups)+1)]
 
-for size, val in groups:
-    for i in range(k-1, size-1, -1):
-        dp[i] = max(dp[i], dp[i-size] + val)
+for i in range(1, len(groups)+1):
+    size, candy = groups[i-1]
+    for j in range(k):
+        if j >= size:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-size] + candy)
+        else:
+            dp[i][j] = dp[i-1][j]
+print(max(dp[len(groups)]))
 
-print(max(dp))
