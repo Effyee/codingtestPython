b6440c80f8a4388581cca6fef7260f427e62e694
Effy__ee <gayean01@gmail.com>
2026-03-19 11:09:32 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3784 ms, Memory: 152244 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,40 +1,42 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().rstrip())) for _ in range(n)]
+n,m=map(int,input().split())
+graph=[list(map(int,input().strip())) for _ in range(n)]
+visited=[[[False,False] for _ in range(m)] for _ in range(n)]
 
-# visited[x][y][0] -> 벽 안 부심 / visited[x][y][1] -> 벽 한 번 부심
-visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
-
-def bfs(x, y, z):
-    q = deque()
-    q.append((x, y, z))
-    visited[x][y][z] = 1
-
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
-
+def bfs():
+    q=deque()
+    visited[0][0][0]=True
+    q.append((0,0,1,0))
     while q:
-        x, y, z = q.popleft()
-        if x == n - 1 and y == m - 1:
-            return visited[x][y][z]
-
+        x,y,dist,flag=q.popleft()
+        if x==n-1 and y==m-1:
+            return dist
+        dx=[0,0,-1,1]
+        dy=[-1,1,0,0]
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if 0<=nx<n and 0<=ny<m:
+                if not flag:
+                    if not visited[nx][ny][0]:
+                        if graph[nx][ny]==0:
+                            visited[nx][ny][0]=True
+                            q.append((nx,ny,dist+1,0))
+                        else:
+                            visited[nx][ny][1]=True
+                            q.append((nx,ny,dist+1,1))
+                else:
+                    if not visited[nx][ny][1] and graph[nx][ny] == 0:
+                        visited[nx][ny][1] = True
+                        q.append((nx, ny, dist + 1, 1))
 
-            if 0 <= nx < n and 0 <= ny < m:
-                # 벽이고 아직 벽 부순 적 없음
-                if graph[nx][ny] == 1 and z == 0 and visited[nx][ny][1] == 0:
-                    visited[nx][ny][1] = visited[x][y][z] + 1
-                    q.append((nx, ny, 1))
-                # 벽이 아니고 방문한 적 없음
-                elif graph[nx][ny] == 0 and visited[nx][ny][z] == 0:
-                    visited[nx][ny][z] = visited[x][y][z] + 1
-                    q.append((nx, ny, z))
+    return int(1e9)
 
-    return -1
-
-print(bfs(0, 0, 0))
+answer=bfs()
+if answer==int(1e9):
+    print(-1)
+else:
+    print(answer)
