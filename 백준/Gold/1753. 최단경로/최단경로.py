a7353e8a985e561f6e795bf0d78e8d5539cd0cd2
Effy__ee <gayean01@gmail.com>
2026-03-20 16:45:11 +0900

    [Gold IV] Title: 최단경로, Time: 832 ms, Memory: 74636 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,34 +1,35 @@
 import sys
 import heapq
-
 input=sys.stdin.readline
 INF=int(1e9)
+
 v,e=map(int,input().split())
-#시작 정점 번호
+distance=[INF]*(v+1)
 k=int(input())
 graph=[[] for _ in range(v+1)]
+
 for _ in range(e):
-    start,end,w=map(int,input().split())
-    graph[start].append((end,w))
+    s,e,dist=map(int,input().split())
+    graph[s].append([e,dist])
 
-distances=[INF]*(v+1)
-def dijkstra(start):
+def dijkstra(k):
     hq=[]
-    heapq.heappush(hq,(0,start))
-    distances[start]=0
+    heapq.heappush(hq,(0,k))
+    distance[k]=0
     while hq:
-        dist,now=heapq.heappop(hq)
-        if distances[now]<dist:
+        dist,node=heapq.heappop(hq)
+        if distance[node]>dist:
             continue
-        for i in graph[now]:
-            cost=dist+i[1]
-            if cost<distances[i[0]]:
-                distances[i[0]]=cost
-                heapq.heappush(hq,(cost,i[0]))
+        for next_node,d in graph[node]:
+            dd=distance[node]+d
+            if distance[next_node]>dd:
+                distance[next_node]=dd
+                heapq.heappush(hq,(dd,next_node))
 
+    return
 dijkstra(k)
-for i in range(1, v+1):
-    if distances[i] == INF:
-        print("INF")
+for i in range(1,len(distance)):
+    if distance[i]==int(1e9):
+        print('INF')
     else:
-        print(distances[i])
+        print(distance[i])
