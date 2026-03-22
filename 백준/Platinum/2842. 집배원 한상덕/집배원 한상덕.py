6ee747b9c37844704f56010695d36ba55ee4d0fb
Effy__ee <gayean01@gmail.com>
2026-03-22 12:23:49 +0900

    [Platinum IV] Title: 집배원 한상덕, Time: 4160 ms, Memory: 35068 KB -BaekjoonHub

--- 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.py
+++ 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.py
@@ -1,73 +1,67 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-# 입력
-n = int(input())
-graph = [list(input().strip()) for _ in range(n)]
-cost = [list(map(int, input().split())) for _ in range(n)]
-
-# 시작점(P), 집(K) 개수 찾기
-home = 0
-sx, sy = 0, 0
-heights = set()
+n=int(input())
+village=[list(map(str,input().strip())) for _ in range(n)]
+cost=[list(map(int,input().split())) for _ in range(n)]
+flat=[cost[i][j] for i in range(n) for j in range(n)]
+height=sorted(list(set(flat)))
+# 배달 시작점
+sx,sy=0,0
+# 집의 갯수
+vc=0
 
 for i in range(n):
     for j in range(n):
-        heights.add(cost[i][j])
-        if graph[i][j] == 'K':
-            home += 1
-        elif graph[i][j] == 'P':
-            sx, sy = i, j
+        if village[i][j]=='P':
+            sx,sy=i,j
+        elif village[i][j]=='K':
+            vc+=1
 
-heights = sorted(heights)
-
-# 8방향
-dx = [-1,-1,-1,0,0,1,1,1]
-dy = [-1,0,1,-1,1,-1,0,1]
-
-# BFS
-def bfs(low, high):
-    # 시작점이 범위 밖이면 불가능
-    if not (low <= cost[sx][sy] <= high):
+def bfs(l,r):
+    if not(l <= (cost[sx][sy]) <= r):
         return False
-
-    visited = [[False]*n for _ in range(n)]
-    q = deque()
-    q.append((sx, sy))
-    visited[sx][sy] = True
-
-    count = 0
-
+    # 초기화
+    dx=[0,0,-1,1,-1,1,-1,1]
+    dy=[-1,1,0,0,-1,-1,1,1]
+    visited=[[False]*n for _ in range(n)]
+    q=deque()
+    visited[sx][sy]=True
+    q.append((sx,sy))
+    count=0
     while q:
-        x, y = q.popleft()
+        x,y=q.popleft()
+        for i in range(8):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            # 방문 안한 칸일 때
+            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+                # 범위 안에 들어오고
+                if l<=(cost[nx][ny])<=r:
+                    visited[nx][ny]=True
+                    # 농지인 경우
+                    if village[nx][ny]=='.':
+                        q.append((nx,ny))
+                    # 집인 경우
+                    elif village[nx][ny]=='K':
+                        count+=1
+                        q.append((nx,ny))
+    return count==vc
 
-        if graph[x][y] == 'K':
-            count += 1
+# 가장 작은 피로도 구하기
+answer=int(1e9)
+def solution():
+    global answer
+    left,right=0,0
+    while left<len(height):
+        while right<len(height):
+            if bfs(height[left],height[right]):
+                answer=min(answer,height[right]-height[left])
+                break
+            right+=1
+        left+=1
+    return
 
-        for d in range(8):
-            nx = x + dx[d]
-            ny = y + dy[d]
-
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                if low <= cost[nx][ny] <= high:
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-
-    return count == home
-
-
-# 투 포인터
-answer = int(1e9)
-left = 0
-right = 0
-
-while left < len(heights):
-    while right < len(heights):
-        if bfs(heights[left], heights[right]):
-            answer = min(answer, heights[right] - heights[left])
-            break
-        right += 1
-    left += 1
-
+solution()
 print(answer)
