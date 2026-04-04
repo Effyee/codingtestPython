f96efe1d9230a8504e952e547acce8ce7ac6ecbe
Effy__ee <gayean01@gmail.com>
2026-04-04 14:24:09 +0900

    [level 2] Title: 더 맵게, Time: 193.62 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 40.9 MB, 시간: 216.70 ms
+메모리: 40.8 MB, 시간: 193.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 03일 13:16:20
+2026년 04월 04일 14:24:04
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -1,22 +1,27 @@
 #include <string>
-#include <vector>
 #include <queue>
 #include <functional>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
     priority_queue<int,vector<int>,greater<int>> pq(scoville.begin(),scoville.end());
-    while (pq.top()<K){
-        if (pq.size() < 2) return -1;
-        int a=pq.top();
+    
+    while(pq.top()<K){
+        if(pq.size()<=1){
+            return -1;
+        }
+        int f=pq.top();
         pq.pop();
-        int b=pq.top();
+        int s=pq.top();
         pq.pop();
-        
-        int c = a + (b * 2);
-        pq.push(c);
+        int t=f+s*2;
+        pq.push(t);
         answer++;
     }
+    
     return answer;
 }
