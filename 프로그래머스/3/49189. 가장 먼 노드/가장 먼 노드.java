eb111a3a4780876f9e0847c2401bde7ca8c66267
Effy__ee <gayean01@gmail.com>
2026-06-04 13:18:58 +0900

    [level 3] Title: 가장 먼 노드, Time: 18.90 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,45 +3,37 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for(int i = 0; i <= n; i++){
-            graph.add(new ArrayList<>());
-        }
-
-        for(int[] e : edge){
-            int a = e[0];
-            int b = e[1];
-
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-        
         int[] distance=new int[n+1];
         Arrays.fill(distance,n+1);
         distance[0]=0;
+        List<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] e : edge) {
+            int num1 = e[0];
+            int num2 = e[1];
+            graph.get(num1).add(num2);
+            graph.get(num2).add(num1);
+        }
         Queue<Integer>q=new LinkedList<>();
         q.offer(1);
         distance[1]=0;
-        
         while(!q.isEmpty()){
             int now=q.poll();
-            for(int next_node:graph.get(now)){
-                if(distance[next_node]>distance[now]+1){
-                    distance[next_node]=distance[now]+1;
-                    q.offer(next_node);                      
+            for(int next:graph.get(now)){
+                if(distance[next]>distance[now]+1){
+                    distance[next]=distance[now]+1;
+                    q.offer(next);
                 }
             }
         }
-        
         Arrays.sort(distance);
-        int max_distance=distance[distance.length-1];
-        
-        for(int dist:distance){
-            if(dist==max_distance){
-                answer++;
-            }
+        int max=distance[n];
+        for(int a:distance){
+            if(max==a){
+            answer++;}
         }
         return answer;
     }
