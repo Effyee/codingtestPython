8510fa27624f0c70c8b41c33f53968485ed57cb3
Effy__ee <gayean01@gmail.com>
2026-06-09 14:03:19 +0900

    [level 2] Title: 비밀 코드 해독, Time: 133.64 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,20 +1,22 @@
 from itertools import combinations
+
 def solution(n, q, ans):
     answer = 0
-    nums=[i for i in range(1,n+1)]
+    nums=[i for i in range(1, n+1)]
+    
     for i in range(len(ans)):
         if ans[i]==0:
             for num in q[i]:
                 if num in nums:
                     nums.remove(num)
-                    
+    
     for c in combinations(nums,5):
         flag=True
-        for i in range(len(ans)):
+        for i in range(len(q)):
             if len(set(c)&set(q[i]))!=ans[i]:
                 flag=False
                 break
         if flag:
-            answer+=1
-        
+            answer+=1                
+    
     return answer
