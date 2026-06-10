bec9a6fd7ba2ce667c2429432970ce4ca9f6faf8
Effy__ee <gayean01@gmail.com>
2026-06-10 13:05:33 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.61 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ 프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=java) 
 
 ### 성능 요약
 
-메모리: 11.4 MB, 시간: 0.04 ms
+메모리: 85.7 MB, 시간: 0.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 10일 12:53:47
+2026년 06월 10일 13:05:29
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    
+    class Node{
+        int time;
+        int cnt;
+        Node(int time, int cnt){
+            this.time=time;
+            this.cnt=cnt;
+        }
+    }
+    
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+        int active=0;
+        Queue<Node> q=new LinkedList<>();
+        
+        for(int player : players){
+
+    while(!q.isEmpty() && q.peek().time == 0){
+        Node n = q.poll();
+        active -= n.cnt;
+    }
+
+    int size = q.size();
+    for(int i = 0; i < size; i++){
+        Node n = q.poll();
+        q.offer(new Node(n.time - 1, n.cnt));
+    }
+
+    int need = player / m;
+
+    if(need > active){
+        int add = need - active;
+
+        answer += add;
+        active += add;
+
+        q.offer(new Node(k - 1, add));
+    }
+}
+        return answer;
+    }
+}
