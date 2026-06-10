6bf5f77acdf2157e32ac6d02c06170022a9ff0b5
Effy__ee <gayean01@gmail.com>
2026-06-10 12:53:50 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -1,31 +1,25 @@
 from collections import deque
-
 def solution(players, m, k):
     answer = 0
-
-    active = 0
-    q = deque()
-
+    q=deque()
+    active=0
+        
     for player in players:
-
-        # 이번 시간 시작 전에 만료 서버 반납
-        while q and q[0][0] == 0:
-            _, cnt = q.popleft()
-            active -= cnt
-
-        # 남은 시간 감소
+        
+        while q and q[0][0]==0:
+            _,cnt=q.popleft()
+            active-=cnt
+        
         for i in range(len(q)):
-            remain, cnt = q.popleft()
-            q.append((remain - 1, cnt))
-
-        need = player // m
-
-        if need > active:
-            add = need - active
-
-            active += add
-            answer += add
-
-            q.append((k - 1, add))
-
+            remain,cnt=q.popleft()
+            q.append((remain-1,cnt))
+            
+        need=player//m
+        
+        if need>active:
+            add=need-active
+            active+=add
+            answer+=add
+            
+            q.append((k-1,add))
     return answer
