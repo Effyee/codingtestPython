4400f76a1b14859777c315b803213e8a5eaa0ca8
Effy__ee <gayean01@gmail.com>
2026-06-11 11:11:12 +0900

    [level 2] Title: 비밀 코드 해독, Time: 139.35 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,13 +1,15 @@
 from itertools import combinations
+
 def solution(n, q, ans):
     answer = 0
     numbers=[i for i in range(1,n+1)]
+    
     for c in combinations(numbers,5):
         flag=True
         for i in range(len(q)):
             if(len(set(q[i])&set(c))!=ans[i]):
                 flag=False
                 break
-        if(flag):
+        if flag:
             answer+=1
     return answer
