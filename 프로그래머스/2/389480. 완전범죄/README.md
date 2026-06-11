94196acf3a827967615a4a565b3b7317bc6fc2ed
Effy__ee <gayean01@gmail.com>
2026-06-11 10:36:39 +0900

    [level 2] Title: 완전범죄, Time: 0.32 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/README.md
+++ 프로그래머스/2/389480. 완전범죄/README.md
@@ -1,10 +1,10 @@
 # [level 2] 완전범죄 - 389480 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389480?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389480?language=java) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 0.24 ms
+메모리: 85.6 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 11일 10:22:15
+2026년 06월 11일 10:36:36
 
 ### 문제 설명
 
--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,30 +1,29 @@
+import java.util.*;
+
 class Solution {
+    static final int INF = 1000000000;
     public int solution(int[][] info, int n, int m) {
-        int INF=1000000000;
+        
         int answer = INF;
         int[] dp=new int[m];
-        
         for(int i=0;i<m;i++){
             dp[i]=INF;
         }
         dp[0]=0;
         
-        for(int[] infos:info){
-            int a_trace=infos[0];
-            int b_trace=infos[1];
-            
+        for(int[] trace: info){
+            int a_trace=trace[0];
+            int b_trace=trace[1];
             int[] new_dp=new int[m];
-            
             for(int i=0;i<m;i++){
                 new_dp[i]=INF;
             }
-            
-            for(int prev_b=0;prev_b<m;prev_b++){
+            for(int prev_b=0; prev_b<m; prev_b++){
                 if(dp[prev_b]==INF){
                     continue;
                 }
                 //A가 훔침
-                int a_new=a_trace+dp[prev_b];
+                int a_new=dp[prev_b]+a_trace;
                 if(a_new<n){
                     new_dp[prev_b]=Math.min(a_new,new_dp[prev_b]);
                 }
@@ -33,12 +32,11 @@ class Solution {
                 if(b_new<m){
                     new_dp[b_new]=Math.min(dp[prev_b],new_dp[b_new]);
                 }
-                
             }
             dp=new_dp;
         }
-        for(int i=0;i<dp.length;i++){
-            if(dp[i]<answer){
+        for(int i=0;i<m;i++){
+            if(answer>dp[i]){
                 answer=dp[i];
             }
         }
