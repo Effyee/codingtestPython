0bd661964f7a6c762eb8a8cd4f54d5a42561abeb
Effy__ee <gayean01@gmail.com>
2026-06-11 11:05:25 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.65 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 class Solution {
-    
     class Node{
         int time;
         int cnt;
@@ -10,36 +9,30 @@ class Solution {
             this.cnt=cnt;
         }
     }
-    
     public int solution(int[] players, int m, int k) {
         int answer = 0;
         int active=0;
         Queue<Node> q=new LinkedList<>();
-        
-        for(int player : players){
+        for(int player:players){
+            while(!q.isEmpty()&& q.peek().time == 0){
+                    active-=q.peek().cnt;
+                    q.poll();
+            }
+            for(int i=0;i<q.size();i++){
+                Node n=q.poll();
+                q.offer(new Node(n.time-1, n.cnt));
+            }
+            
+            int need=player/m;
+            if(need>active){
+                int add=need-active;
+                
+                answer += add;
+                active += add;
 
-    while(!q.isEmpty() && q.peek().time == 0){
-        Node n = q.poll();
-        active -= n.cnt;
-    }
-
-    int size = q.size();
-    for(int i = 0; i < size; i++){
-        Node n = q.poll();
-        q.offer(new Node(n.time - 1, n.cnt));
-    }
-
-    int need = player / m;
-
-    if(need > active){
-        int add = need - active;
-
-        answer += add;
-        active += add;
-
-        q.offer(new Node(k - 1, add));
-    }
-}
+                q.offer(new Node(k - 1, add));
+            }
+        }
         return answer;
     }
 }
