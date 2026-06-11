99cb19791f6d611110be75066fe12101b785089e
Effy__ee <gayean01@gmail.com>
2026-06-11 10:22:20 +0900

    [level 2] Title: 완전범죄, Time: 0.24 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,22 +1,21 @@
 def solution(info, n, m):
     answer = 0
     INF=int(1e9)
-    dp=[INF]*(m)
+    dp=[INF]*m
     dp[0]=0
-    for a_trace,b_trace in info:
-        new_dp=[INF]*(m)
+    for a_trace, b_trace in info:
+        new_dp=[INF]*m
         for prev_b in range(m):
-            if dp[prev_b]==INF:
+            if(dp[prev_b]==INF):
                 continue
-            #A가 훔침
-            a_new=dp[prev_b]+a_trace
-            if a_new<n:
-                new_dp[prev_b]=min(a_new, new_dp[prev_b])
-            #B가 훔침
-            b_new=prev_b+b_trace
-            if b_new<m:
-                new_dp[b_new]=min(new_dp[b_new], dp[prev_b])
+            new_a=dp[prev_b]+a_trace
+            if(new_a<n):
+                new_dp[prev_b]=min(new_a, new_dp[prev_b])
+            new_b=prev_b+b_trace
+            if(new_b<m):
+                new_dp[new_b]=min(dp[prev_b],new_dp[new_b])
         dp=new_dp
-    if min(dp)==INF:
+    answer=min(dp)
+    if(answer==INF):
         return -1
-    return min(dp)
+    return answer
